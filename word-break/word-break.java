2bdec15e2b7c2013da0bd717f7c5f56c7c66dafa
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2021-07-24 18:25:26 -0700

    Time: 7 ms (45.40%), Space: 39.5 MB (32.01%) - LeetHub

---
+++ word-break/word-break.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+      boolean [] dp = new boolean[s.length()+1];
+        dp[0]=true;
+        for(int i =1; i<dp.length; i++){
+            for(int j=0; j<s.length(); j++){
+                if(dp[j]==true && wordDict.contains(s.substring(j,i))){
+                    dp[i]=true;
+                }
+            }
+        }
+        return dp[dp.length-1];
+    }
+}
