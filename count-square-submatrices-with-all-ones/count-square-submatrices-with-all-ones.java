2e5ef43621327f92c470e384ae371cf7228d24f2
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2021-08-01 12:03:37 -0700

    Time: 5 ms (92.59%), Space: 51.9 MB (23.40%) - LeetHub

---
+++ count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int countSquares(int[][] matrix) {
+         int m=matrix.length;
+        int n=matrix[0].length;
+        int count=0;
+        int[][] dp= new int[m][n];
+        for(int i=0;i<m;i++){
+            dp[i][0]=matrix[i][0];
+            count+=dp[i][0];
+        }
+        for(int i=1;i<n;i++){
+            dp[0][i]=matrix[0][i];
+            count+=dp[0][i];    
+        }
+        
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][j]==1){
+                    dp[i][j]=1+Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1]));
+                count+=dp[i][j];
+                }
+            }
+
+        }
+        return count;
+    }
+}
