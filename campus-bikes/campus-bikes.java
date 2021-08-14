83b5bb21e1c263c628cee8eb58aa33fac62d2c25
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2021-08-13 23:46:38 -0700

    Time: 85 ms (72.10%), Space: 85 MB (57.14%) - LeetHub

--- campus-bikes/campus-bikes.java
+++ campus-bikes/campus-bikes.java
@@ -1,37 +1,45 @@
 class Solution {
     public int[] assignBikes(int[][] workers, int[][] bikes) {
-          
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[2] != b[2] ?  a[2] - b[2] : (a[0] !=  b[0] ? a[0] - b[0] : a[1] - b[1]));
-        
-        for(int i=0; i < workers.length; i++){
-            for(int j=0; j < bikes.length; j++){
-                pq.offer(new int[]{i, j, dist(workers[i], bikes[j])});  // worker, bike, distance
-            }
-        }
-                 
-        
-        int[] workerStatus = new int[workers.length];
-        int[] bikeStatus = new int[bikes.length];
-        
-        int[] res = new int[workers.length];
-        
-        int assigned = 0;
-        while(assigned < workers.length){
-            int[] a = pq.poll();
-          //  System.out.println(a[0]+" "+a[1]+ " "+ a[2]);
-            if(workerStatus[a[0]] == 0 && bikeStatus[a[1]] == 0){
-                res[a[0]] = a[1];
-                
-                workerStatus[a[0]] = 1;
-                bikeStatus[a[1]] = 1;
-                assigned++;
-            }
-        }
-              
-        return res;
+          int[] result = new int[workers.length];
+     Set<Integer> workerSet = new HashSet<Integer>();
+     Set<Integer> bikeSet = new HashSet<Integer>();
+    
+    for(int i =0; i<workers.length ;i++ ){
+        workerSet.add(i);
+    }
+    for(int i =0; i<bikes.length ;i++ ){
+        bikeSet.add(i);
     }
     
-    private int dist(int[] a, int[] b){
-        return Math.abs(a[0] - b[0]) + Math.abs(a[1] - b[1]);
+    Map<Integer, List> map = new HashMap<Integer, List>();
+    
+    for(int i =0; i<workers.length ;i++ ){
+        for(int j =0; j<bikes.length ;j++){
+            int distance = Math.abs(workers[i][0] - bikes[j][0]) + Math.abs(workers[i][1] - bikes[j][1]);
+            if(!map.containsKey(distance)){
+                List<int[]> list = new ArrayList<int[]>();
+                list.add(new int[]{i,j});
+                map.put(distance, list);
+            }else{
+                map.get(distance).add(new int[]{i,j});
+            }
+        }
+    }
+    
+    for(int i =1; i<2000; i++){
+        if(map.get(i)!=null){
+            List<int[]> list = map.get(i);
+            for(int j=0; j<list.size();j++){
+                int[] pair  = list.get(j);
+                if(workerSet.contains(pair[0]) && bikeSet.contains(pair[1])){
+                    result[pair[0]] = pair[1];
+                    workerSet.remove(pair[0]);
+                    bikeSet.remove(pair[1]);
+                }
+            }
+        }
+    }
+    
+    return result;
     }
 } 
