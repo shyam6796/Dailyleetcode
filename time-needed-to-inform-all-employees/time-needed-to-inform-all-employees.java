f0c106bc1e054216fb08573cc5d26e0a23ac91c2
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2021-08-15 20:14:33 -0700

    Time: 132 ms (31.20%), Space: 63.4 MB (28.36%) - LeetHub

---
+++ time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
+        if(n==1)return informTime[0];
+        int time[]  = new int[n];
+        HashMap<Integer,List<Integer>> map =new HashMap<>();
+        for(int i=0; i<manager.length; i++){
+             if(manager[i]==-1 )continue;
+            if(!map.containsKey(manager[i])){
+                map.put(manager[i],new ArrayList<>());
+            }
+            map.get(manager[i]).add(i);
+        }
+        //System.out.println(map);
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(headID);
+        
+        while(!queue.isEmpty()){
+            int v = queue.poll();
+            int infoTime = informTime[v];
+            if(map.get(v) != null){
+                for(int u : map.get(v)){
+                    time[u] = time[v] + infoTime;
+                    queue.add(u);
+                }
+            }
+        }
+        
+         int maxTime = 0;
+        for(int t : time)
+            maxTime = Math.max(t, maxTime);
+        return maxTime;
+       
+    }
+}
