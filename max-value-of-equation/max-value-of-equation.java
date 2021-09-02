d973d3fa3de27994a4ed4780e0a9b8feb676fe48
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2021-09-02 08:34:11 -0700

    Time: 46 ms (6.01%), Space: 139.3 MB (5.06%) - LeetHub

--- max-value-of-equation/max-value-of-equation.java
+++ max-value-of-equation/max-value-of-equation.java
@@ -1,15 +1,17 @@
 class Solution {
     public int findMaxValueOfEquation(int[][] points, int k) {
-         PriorityQueue<Pair<Integer, Integer>> maxHeap = new PriorityQueue<>((a, b) -> b.getKey() - a.getKey());
-        int max = Integer.MIN_VALUE;
-        for(int[] point : points) {
-            while(!maxHeap.isEmpty() && point[0] - maxHeap.peek().getValue() > k) {
-                maxHeap.poll();
+        PriorityQueue<Pair<Integer,Integer>>queue = new PriorityQueue<>((a,b) -> b.getKey()- a.getKey());
+        int max =Integer.MIN_VALUE;
+        
+        for(int [] point : points){
+            while(!queue.isEmpty() &&  point[0] - queue.peek().getValue() >k){
+                queue.poll();
             }
-            if(!maxHeap.isEmpty()) {
-                max = Math.max(max, point[0] + point[1] + maxHeap.peek().getKey());
+            if(!queue.isEmpty()){
+                max = Math.max(max, point[0] + point[1] + queue.peek().getKey());
             }
-            maxHeap.offer(new Pair<Integer, Integer>(point[1] - point[0], point[0]));
+            
+            queue.add(new Pair<Integer, Integer>(point[1]- point[0], point[0]));
         }
         return max;
     }
