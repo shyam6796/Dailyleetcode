8d7720fb533781e37f2328af17531a64ecbd4137
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2021-09-05 16:52:47 -0700

    Time: 2218 ms (20.86%), Space: 36.4 MB (49.41%) - LeetHub

---
+++ sentence-screen-fitting/sentence-screen-fitting.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int wordsTyping(String[] sentence, int rows, int cols) {
+        int count = 0;
+        int rowIndex = 0;
+        int wordIndex = 0;
+        int spaces = cols;
+        while(rowIndex < rows){
+            if(sentence[wordIndex].length() <= spaces){
+                spaces -= sentence[wordIndex].length();
+                spaces--; //-1 means extra'-'
+                wordIndex++;
+            }
+            else{
+                rowIndex++;
+                spaces = cols;
+            }
+            if(wordIndex == sentence.length){
+                count++;
+                wordIndex = 0;
+            }
+        }
+        return count;
+    }
+}
