7413362d375c20cf39dfd6726c828bc4a103001a
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2021-10-24 21:13:16 -0700

    Time: 63 ms (26.82%), Space: 78.8 MB (32.38%) - LeetHub

---
+++ course-schedule-iii/course-schedule-iii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int scheduleCourse(int[][] courses) {
+            Arrays.sort(courses,(a,b) ->  a[1]==b[1] ? a[0]-b[0] : a[1] -b[1]);
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b-a);
+        int time=0;
+        
+        for(int [] course : courses){
+            if(course[0] <= course[1]){
+                if(course[0] +time <= course[1]){
+                    time +=course[0];
+                    pq.offer(course[0]);
+                }else{
+                        if(pq.peek() >course[0]){
+                            time -=pq.poll();
+                            time += course[0];
+                            pq.add(course[0]);
+                        }
+                }
+            }
+        }
+        return pq.size();
+    }
+}
