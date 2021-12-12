e87c6f6b388398cdaf5e93fe7ee95aef25dfbc95
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2021-12-11 17:49:31 -0700

    Time: 0 ms (100.00%), Space: 36.9 MB (42.05%) - LeetHub

--- binary-tree-upside-down/binary-tree-upside-down.java
+++ binary-tree-upside-down/binary-tree-upside-down.java
@@ -15,13 +15,15 @@
  */
 class Solution {
     public TreeNode upsideDownBinaryTree(TreeNode root) {
-          if(root == null || (root.left == null && root.right == null))
-            return root;
-        TreeNode newRoot = upsideDownBinaryTree(root.left);
-        root.left.left = root.right;
-        root.left.right = root;
-        root.left = null;
-        root.right = null;
+         if(root == null || (root.left ==null && root.right==null)){
+             return root;
+         }
+         TreeNode newRoot = upsideDownBinaryTree(root.left);
+        root.left.left=root.right;
+        root.left.right=root;
+        root.left =null;
+        root.right=null;
+        
         return newRoot;
     }
 }
