649b9a12c6e37a9f30574ab57afd3967ab14ead1
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2021-12-13 21:14:39 -0700

    Time: 73 ms (36.91%), Space: 35.5 MB (89.00%) - LeetHub

--- beautiful-arrangement/beautiful-arrangement.java
+++ beautiful-arrangement/beautiful-arrangement.java
@@ -1,18 +1,22 @@
-public class Solution {
-    int count = 0;
-    public int countArrangement(int N) {
-        boolean[] visited = new boolean[N + 1];
-        calculate(N, 1, visited);
+class Solution {
+    int count =0;
+    public int countArrangement(int n) {
+        boolean arr[] = new boolean[n+1];
+        helper(arr,1,n);
         return count;
     }
-    public void calculate(int N, int pos, boolean[] visited) {
-        if (pos > N)
+    
+    public void helper(boolean arr[], int pos, int n){
+        if(pos >n){
             count++;
-        for (int i = 1; i <= N; i++) {
-            if (!visited[i] && (pos % i == 0 || i % pos == 0)) {
-                visited[i] = true;
-                calculate(N, pos + 1, visited);
-                visited[i] = false;
+            
+        }
+        
+        for(int i=1; i<=n; i++){
+            if(!arr[i] && (pos%i ==0 || i%pos ==0 )){
+                arr[i] =true;
+                helper(arr,pos+1,n);
+                arr[i] =false;
             }
         }
     }
