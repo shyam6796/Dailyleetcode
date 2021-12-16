0f2aaf13fac1a2c14296d8cf51e274182d803578
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2021-12-15 23:32:42 -0700

    Time: 0 ms (100.00%), Space: 38.7 MB (97.91%) - LeetHub

--- sort-transformed-array/sort-transformed-array.java
+++ sort-transformed-array/sort-transformed-array.java
@@ -1,12 +1,31 @@
 class Solution {
     public int[] sortTransformedArray(int[] nums, int a, int b, int c) {
-            for(int i=0; i<nums.length; i++){
-                
-                nums[i] = a*(nums[i]*nums[i]) + (b*nums[i]) +c;
-               
-            }
+           for(int i=0; i<nums.length; i++){
+               nums[i] =nums[i] * nums[i] * a + nums[i] * b + c;
+           }
+        int [] ans = new int[nums.length];
         
-        Arrays.sort(nums);
-        return nums;
+        if(a>=0){
+            int index = nums.length-1;
+            int i=0, j= index;
+            while(index>=0){
+                if(nums[i] >=nums[j]){
+                    ans[index--] = nums[i++];
+                }else{
+                    ans[index--] = nums[j--];
+                } 
+            }
+        }else{
+            int index = 0;
+            int i=0, j = nums.length-1;
+            while(index<=nums.length-1){
+                if(nums[i] >= nums[j]){
+                    ans[index++] = nums[j--];
+                }else{
+                    ans[index++] = nums[i++];
+                } 
+            }
+        }
+         return ans;   
     }
-}
+}   
