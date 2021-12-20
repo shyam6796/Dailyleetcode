178379d05079a967f3c36d4d5c7d3d575c515ea6
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2021-12-19 19:03:49 -0700

    Time: 42 ms (87.06%), Space: 54.8 MB (88.26%) - LeetHub

--- all-oone-data-structure/all-oone-data-structure.java
+++ all-oone-data-structure/all-oone-data-structure.java
@@ -1,67 +1,68 @@
 class AllOne {
     HashMap<String,Integer> map;
-     TreeMap<Integer, List<String>> treeMap;
+    TreeMap<Integer,List<String>> treeMap;
     public AllOne() {
         map = new HashMap<>();
         treeMap = new TreeMap<>();
     }
     
     public void inc(String key) {
-       
+        
         if(map.containsKey(key)){
-             int oldCnt = map.get(key);
-             removeCount(key, oldCnt);
-             updateCount(key, oldCnt+1);
-        }else   updateCount(key, 1);
+            int oldCount = map.get(key);
+            removeKey(key,oldCount);
+            updateKey(key,oldCount+1);
+        }else{
+            updateKey(key,1);
+        }
     }
     
     public void dec(String key) {
-         if(!map.containsKey(key)){
-             return ;
+        if(!map.containsKey(key)){
+            return;
         }
-        int oldCnt = map.get(key);
-
-		removeCount(key, oldCnt);
-        	if (oldCnt == 1) {
-			map.remove(key);
-		} else {
-			updateCount(key, oldCnt - 1);
-		}
+        int oldCount = map.get(key);
+        removeKey(key,oldCount); 
+        if(oldCount==1){
+            map.remove(key);        
+        }else{
+             updateKey(key,oldCount-1);
+        }
+    
+       
+        
+        
+    }
+    
+    public void removeKey(String key, int count){
+        List<String> list = treeMap.get(count);
+        list.remove(key);
+        if (list.size() == 0) {
+			treeMap.remove(count);
+        } 
+    }
+    
+    public void updateKey(String key, int count){
+        map.put(key,count);
+        if(!treeMap.containsKey(count)){
+            treeMap.put(count, new ArrayList<>());
+        }
+        treeMap.get(count).add(key);
     }
     
     public String getMaxKey() {
         if (treeMap.size() == 0) {
 			return "";
 		}
-      return  treeMap.lastEntry().getValue().get(0); 
+      return  treeMap.lastEntry().getValue().get(0);
     }
     
     public String getMinKey() {
         if (treeMap.size() == 0) {
 			return "";
 		}
-       return  treeMap.firstEntry().getValue().get(0); 
+        return treeMap.firstEntry().getValue().get(0);
     }
-    
-    public void removeCount(String key, int count){
-        List<String> list = treeMap.get(count);
-		list.remove(key);
-		if (list.size() == 0) {
-			treeMap.remove(count);
-        } 
-    }
-    
-    public void updateCount(String key, int count){
-        map.put(key,count);
-        
-        if(!treeMap.containsKey(count)) treeMap.put(count,new ArrayList<>());
-        treeMap.get(count).add(key);        
-    
-    
-    }
-    
-    
-    
 }
 
 /**
