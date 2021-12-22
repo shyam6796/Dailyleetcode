641a50a493acbc13bdbb3811457694444aa331df
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2021-12-21 18:15:24 -0700

    Time: 9 ms (99.30%), Space: 38.9 MB (98.70%) - LeetHub

--- design-bounded-blocking-queue/design-bounded-blocking-queue.java
+++ design-bounded-blocking-queue/design-bounded-blocking-queue.java
@@ -1,27 +1,30 @@
 class BoundedBlockingQueue {
-   Queue<Integer> queue;
+    Queue<Integer> queue; 
     int max;
     public BoundedBlockingQueue(int capacity) {
-        queue= new LinkedList<>();
-        max=capacity;
+        queue = new LinkedList<>();
+        max = capacity;
     }
     
     public void enqueue(int element) throws InterruptedException {
         synchronized(queue){
-        while(queue.size() == max)queue.wait();
-        queue.add(element);
-        queue.notify();
-        }
-       
-        
+            while(queue.size() == max){
+                queue.wait();
+                
+            }
+            queue.add(element);
+            queue.notify();
+        } 
     }
     
     public int dequeue() throws InterruptedException {
-         synchronized(queue){
-        while(queue.size() <= 0 )queue.wait();
-        queue.notify();
-        return queue.poll();
-       
+        synchronized(queue){
+            while(queue.size() ==0){
+                queue.wait();
+                
+            }
+            queue.notify();
+           return queue.poll();
         }
     }
     
