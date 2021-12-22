f50de918c438bd542ab7c1dca853f4f3e4861871
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2021-12-21 19:58:44 -0700

    Time: 54 ms (64.22%), Space: 49.9 MB (36.09%) - LeetHub

--- count-different-palindromic-subsequences/count-different-palindromic-subsequences.java
+++ count-different-palindromic-subsequences/count-different-palindromic-subsequences.java
@@ -1,24 +1,24 @@
 class Solution {
     public int countPalindromicSubsequences(String s) {
-        int n=s.length();
-        char [] arr = s.toCharArray();
-        long [][] dp = new long[n][n];
-        long mod =1000000007;
+        long mod =1_000_000_007;
+        char [] str = s.toCharArray();
+        int n =s.length();
+        long dp [][] = new long[n][n];
         
         for(int i=n-1; i>=0; i--){
             dp[i][i]=1;
             for(int j=i+1; j<n; j++){
-                
-                if(arr[i] != arr[j]){
-                    dp[i][j] =dp[i][j-1] +dp[i+1][j] -dp[i+1][j-1];
+                if(str[i] != str[j]){
+                    dp[i][j] =dp[i+1][j] +dp[i][j-1] - dp[i+1][j-1];
                 }else{
                     dp[i][j] = dp[i+1][j-1]*2;
                     
-                    int left =i+1, right =j-1;
-                    while(left <=right && arr[left]  !=arr[i]){
+                    int left =i+1;
+                    int right =j-1;
+                    while(left<=right && str[left] != str[i]){
                         left++;
                     }
-                    while(left <=right && arr[right] !=arr[i]){
+                    while(left<=right && str[right] != str[i]){
                         right--;
                     }
                     if(left >right){
@@ -26,11 +26,13 @@ class Solution {
                     }else if(left ==right){
                         dp[i][j] +=1;
                     }else{
-                        dp[i][j] -= dp[left+1][right-1];
+                        dp[i][j] -=dp[left+1][right-1];
                     }
+                    
                 }
-                dp[i][j] =(dp[i][j]+mod)%mod;
+            dp[i][j] = (dp[i][j]+mod)%mod;
             }
+            
         }
         return (int)dp[0][n-1];
     }
