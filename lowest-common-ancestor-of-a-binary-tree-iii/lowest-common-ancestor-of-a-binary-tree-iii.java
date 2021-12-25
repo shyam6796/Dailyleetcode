a5f29c1379903b7f1089c3ed742a5740b24da54d
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2021-12-25 13:08:35 -0700

    Time: 22 ms (45.09%), Space: 39.3 MB (98.60%) - LeetHub

--- lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.java
+++ lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.java
@@ -10,19 +10,17 @@ class Node {
 
 class Solution {
     public Node lowestCommonAncestor(Node p, Node q) {
-         Set<Node> set = new HashSet<>();
+        Set<Node> set = new HashSet<>();
         
         while(p != null){
             set.add(p);
             p=p.parent;
         }
-        
         while(q != null){
             if(set.contains(q)){
                 return q;
             }
             q= q.parent;
-            
         }
         return null;
     }
