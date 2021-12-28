e04e2c00c6c9fdab3ad7e86a95546943b4ee1f02
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2021-12-27 19:09:20 -0700

    Time: 72 ms (55.85%), Space: 42 MB (48.90%) - LeetHub

--- word-ladder/word-ladder.java
+++ word-ladder/word-ladder.java
@@ -1,37 +1,39 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Set<String> set =new HashSet<>(wordList);
-         Set<String> seen = new HashSet<>();
-        if(!set.contains(endWord))return 0;
-       
-        Queue<String> queue= new LinkedList<>();
+        Set<String> set = new HashSet<>(wordList);
+        Set<String> seen = new HashSet<>();
+        if(!wordList.contains(endWord))return 0;
+        Queue<String> queue = new LinkedList<>();
         queue.add(beginWord);
         seen.add(beginWord);
-        int level = 1;
+        int level =1;
+        
         while(!queue.isEmpty()){
-            int size= queue.size();
-            for(int i=0; i<size; i++){
-                String word=queue.poll();
-                char w[] =word.toCharArray();
-                for(int j=0; j<w.length; j++){
-                    char c =w[j];
-                    for(char a ='a'; a<='z'; a++){
-                        if(a==c)continue;
-                        w[j]=a;
-                        String newWord =String.valueOf(w);
-                        if(newWord.equals(endWord)){
-                            return level+1;
+            int n =queue.size();
+            for(int i=0; i<n; i++){
+                String s =queue.poll();
+               // System.out.println(s);
+                char arr[]= s.toCharArray();
+                for(int k=0; k<arr.length; k++){
+                    char c =arr[k];
+                    for(char j='a'; j<='z'; j++){
+                        if(c==j)continue;
+                        arr[k]=j;
+                        String newWord =String.valueOf(arr);
+                        if(newWord.equals(endWord))return level+1;
+                        if(set.contains(newWord) && !seen.contains(newWord)){
+                                seen.add(newWord);
+                                queue.add(newWord);
                         }
-                         if(!seen.contains(newWord) && set.contains(newWord) ){
-                             queue.add(newWord);
-                            seen.add(newWord);
-                        }
+                        
+                       
                     }
-                    w[j]=c;
-                }    
+                     arr[k]=c;
+                }
             }
             level++;
         }
-         return 0;
+        return 0;
+        
     }
 }
