bceb9691c0703cd69db16e863534e634da7a61d2
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2021-12-29 23:28:27 -0700

    Time: 93 ms (7.37%), Space: 94.1 MB (20.74%) - LeetHub

---
+++ insert-delete-getrandom-o1-duplicates-allowed/insert-delete-getrandom-o1-duplicates-allowed.java
@@ -0,0 +1,44 @@
+class RandomizedCollection {
+    List<Integer> list;
+     HashMap<Integer, Set<Integer>> idx;
+    Random rand = new Random();
+    public RandomizedCollection() {
+        list= new ArrayList<>();
+        idx = new HashMap<Integer, Set<Integer>>();
+    }
+    
+    public boolean insert(int val) {
+        if(!idx.containsKey(val))idx.put(val ,new LinkedHashSet<>());
+        idx.get(val).add(list.size());
+        list.add(val);
+        return idx.get(val).size() ==1;
+    }
+    
+    public boolean remove(int val) {
+         if (!idx.containsKey(val) || idx.get(val).size() == 0) return false;
+           int remove_idx = idx.get(val).iterator().next(); 
+            idx.get(val).remove(remove_idx);
+            int last = list.get(list.size() - 1);
+            list.set(remove_idx,last);
+            idx.get(last).add(remove_idx);
+            idx.get(last).remove(list.size()-1);
+        
+            list.remove(list.size() - 1);
+        
+            return true;
+        
+    }
+    
+    public int getRandom() {
+        int index =rand.nextInt(list.size());
+        return list.get(index);
+    }
+}
+
+/**
+ * Your RandomizedCollection object will be instantiated and called as such:
+ * RandomizedCollection obj = new RandomizedCollection();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
