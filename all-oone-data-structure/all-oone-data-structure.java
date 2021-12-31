2a0f37c4bd72b9b851377f5089c86596c5e7621d
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2021-12-31 16:18:53 -0700

    Time: 92 ms (19.16%), Space: 83.4 MB (16.17%) - LeetHub

--- all-oone-data-structure/all-oone-data-structure.java
+++ all-oone-data-structure/all-oone-data-structure.java
@@ -64,12 +64,3 @@ class AllOne {
         return treeMap.firstEntry().getValue().get(0);
     }
 }
-
-/**
- * Your AllOne object will be instantiated and called as such:
- * AllOne obj = new AllOne();
- * obj.inc(key);
- * obj.dec(key);
- * String param_3 = obj.getMaxKey();
- * String param_4 = obj.getMinKey();
- */
