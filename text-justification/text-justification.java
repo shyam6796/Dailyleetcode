e7c207c625003f44c42ccfec577e6c864e911776
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2021-12-31 16:54:44 -0700

    Time: 0 ms (100.00%), Space: 39.1 MB (18.02%) - LeetHub

--- text-justification/text-justification.java
+++ text-justification/text-justification.java
@@ -1,22 +1,22 @@
 class Solution {
     public List<String> fullJustify(String[] words, int maxWidth) {
-        List<String> res = new ArrayList<>();
+        List<String> list = new ArrayList<>();
         int n =words.length;
-        int index=0;
+        int index =0;
         
-        while(index <n){
-            int totalChars = words[index].length();
-            int last = index+1;
+        while(index<n){
+            int totalChar = words[index].length();
+            int last =index+1;
             
-            while(last< n){
-               if(totalChars + 1 + words[last].length() >maxWidth) break;
-                totalChars += 1 + words[last].length();
+            while(last<n){
+                if(totalChar + 1+ words[last].length() >maxWidth )break;
+                totalChar += 1+ words[last].length();
                 last++;
             }
-            int gaps = last -index -1;
-            StringBuilder sb = new StringBuilder();
             
-            if(last==n || gaps==0){
+            int gaps = last -index-1;
+            StringBuilder sb = new StringBuilder();
+            if(last ==n || gaps==0){
                 
                 for(int i=index; i<last; i++){
                     sb.append(words[i]);
@@ -27,27 +27,24 @@ class Solution {
                 while(sb.length()<maxWidth){
                     sb.append(' ');
                 }
-                
             }else{
                 
-                int space = (maxWidth -totalChars)/gaps;
-                int rest = (maxWidth -totalChars)%gaps;
+                int space =(maxWidth- totalChar)/gaps;
+                int extra  =(maxWidth- totalChar)%gaps;
                 
-                for(int i=index; i<last-1; i++){
+                for(int i= index; i<last-1; i++){
                     sb.append(words[i]);
                     sb.append(' ');
                     
-                    for(int j=0; j< space + (rest > i-index ?1 :0); j++){
-                           sb.append(' '); 
+                    for(int j=0; j<space + (extra > i-index ?1:0); j++){
+                        sb.append(' ');
                     }
-                    
                 }
                 sb.append(words[last-1]);
             }
             index =last;
-            res.add(sb.toString());
+            list.add(sb.toString());
         }
-        
-        return res;
+        return list;
     }
 }
