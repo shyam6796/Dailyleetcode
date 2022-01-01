2673cc3a8c6dbcb7fe43a31499d5b566ace64957
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2021-12-31 18:39:22 -0700

    Time: 10 ms (66.52%), Space: 51.5 MB (7.89%) - LeetHub

--- factor-combinations/factor-combinations.java
+++ factor-combinations/factor-combinations.java
@@ -2,28 +2,30 @@ class Solution {
     public List<List<Integer>> getFactors(int n) {
         
         List<List<Integer>> list = new ArrayList<>();
-        if(n<=3)return list;
+        if(n<=3) return list;
+       
+        helper(n,2,list, new ArrayList<>());
         
-        helper(n,2,list,new ArrayList<>());
-        return list;
+        return list; 
     }
     
-    public void helper( int n, int factor,List<List<Integer>> list,List<Integer> ans){
+    public void  helper(int n, int factor,List<List<Integer>> list, List<Integer> ans){
         
-        for(int i=factor; i*i<=n; i++){
+        for(int i= factor; i*i<=n; i++){
             if(n%i !=0){
                 continue;
-            }   
+            }
+            
             ans.add(i);
             List<Integer> temp = new ArrayList<>(ans);
             temp.add(n/i);
             list.add(temp);
             
-            if(i *i <=n/i){
-                helper(n/i,i,list,ans);
+            if(i*i <= n/i){
+                helper(n/i, i, list,ans);
             }
             ans.remove(ans.size()-1);
-            
         }
+        
     }
 }
