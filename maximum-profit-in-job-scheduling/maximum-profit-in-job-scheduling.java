699463f426d9d9d7ddd5346e88d3ee861f71c766
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2022-01-01 15:07:36 -0700

    Time: 39 ms (51.26%), Space: 48 MB (78.84%) - LeetHub

--- maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.java
+++ maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.java
@@ -1,24 +1,22 @@
 class Solution {
     public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
-        int n  = profit.length;
-        int [][] jobs =new int[n][3];
-        TreeMap<Integer,Integer> map = new TreeMap<>();
-        map.put(0,0);
+        int n = startTime.length;
+        int[][] dp=new int[n][3];
+        TreeMap<Integer,Integer> map =new TreeMap<>();
         for(int i=0; i<n; i++){
-            jobs[i][0] =startTime[i];
-            jobs[i][1] =endTime[i];
-            jobs[i][2] =profit[i];
-            
+            dp[i][0]=startTime[i];
+            dp[i][1]=endTime[i];
+            dp[i][2]=profit[i];
         }
-        Arrays.sort(jobs,(a,b) -> a[1] -b[1]);
-        
-        for(int [] job : jobs){
-            int val =job[2] + map.floorEntry(job[0]).getValue();
-            if(val >map.lastEntry().getValue()){
-                map.put(job[1],val);
+        Arrays.sort(dp, (a,b) -> a[1]-b[1]);
+        map.put(0,0);
+        for(int curr[] : dp){
+            int profits = map.floorEntry(curr[0]).getValue() +curr[2];
+            if(map.lastEntry().getValue()< profits){
+                map.put(curr[1],profits);
             }
-            
         }
+        
         return map.lastEntry().getValue();
     }
 }
