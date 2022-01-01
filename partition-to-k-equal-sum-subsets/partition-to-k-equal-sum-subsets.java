53b366fc946fbc265eb192e2f839776202a00ab4
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2021-12-31 21:43:18 -0700

    Time: 153 ms (10.66%), Space: 38.7 MB (17.66%) - LeetHub

--- partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.java
+++ partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.java
@@ -1,43 +1,41 @@
 class Solution {
     public boolean canPartitionKSubsets(int[] nums, int k) {
         int sum =0;
-        for(int i : nums){
-            sum +=i;
+        for(int i :nums){
+           sum +=i; 
         }
+        
         if(sum %k !=0)return false;
-        
-        int target =sum /k;
         Arrays.sort(nums);
-        if(target < nums[nums.length-1]) return false;
-        
-        boolean arr[] = new boolean[nums.length];
-        
-        return dfs(arr,nums,target,k,0,0,0);
-        
+        sum =sum/k;
+        if(sum <nums[nums.length-1])return false;
+        boolean[] arr =new boolean[nums.length];
+        return helper(nums,k,sum,arr,0,0,0);
     }
     
-    
-    public boolean dfs(boolean[] arr, int[] nums, int target,int targetSet, int idx, int currentSum, int currentSet){
-        if(currentSet== targetSet) return true;
+    public boolean helper(int []nums, int targetSet, int targetSum, boolean [] arr, int idx, int currentSum, int curentSet){
+        if(curentSet == targetSet){
+              return true;              
+        }
         
         for(int i=idx; i<nums.length; i++){
             
-            if(arr[i] || (i <nums.length -1 && (nums[i] ==nums[i+1]) && arr[i+1]==true)){
+            if(arr[i] || (i<nums.length-1 && nums[i]==nums[i+1] && arr[i+1]==true)){
                 continue;
             }
-        
-            int sum = currentSum + nums[i];
-            if(sum > target)break;
+            
+            int sum = currentSum +nums[i];
+            if(sum > targetSum)break;
             
             arr[i] =true;
-            if(sum ==target && dfs(arr,nums,target,targetSet,0,0, currentSet+1)){
+            if(sum== targetSum && helper(nums,targetSet,targetSum,arr,0,0,curentSet+1)){
                 return true;
-            }else if(dfs(arr,nums,target,targetSet,idx+1, sum, currentSet)){
+            }else if(helper(nums,targetSet,targetSum,arr,idx+1,sum,curentSet)){
                 return true;
             }
              arr[i] =false;
+            
         }
-        
         return false;
     }
 }
