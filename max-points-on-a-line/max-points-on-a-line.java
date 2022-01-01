184edc28942e2604b54de67e6e9cb0564b073e37
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2021-12-31 20:37:51 -0700

    Time: 19 ms (32.37%), Space: 41.2 MB (12.10%) - LeetHub

--- max-points-on-a-line/max-points-on-a-line.java
+++ max-points-on-a-line/max-points-on-a-line.java
@@ -1,36 +1,31 @@
 class Solution {
     public int maxPoints(int[][] points) {
-         if(points.length <3)return points.length;
+        int max=1;
         
-        int pointsOnLine =2;
-        
-        for(int i=0; i< points.length; i++){
-              HashMap<String,Integer> map = new HashMap<>();
+        for(int i=0; i<points.length; i++){
+            HashMap<String,Integer> map = new HashMap<>();
             for(int j=i+1; j<points.length; j++){
-                String s= getSlope(points[i],points[j]);
-                map.put(s, map.getOrDefault(s,0)+1);
+                String slope =getSlope(points[i], points[j]);
+                map.put(slope, map.getOrDefault(slope,0)+1);
             }
-        for(String s :map.keySet()){
-            pointsOnLine =Math.max(pointsOnLine, map.get(s)+1);
-            }    
-            
+            for(String s :map.keySet()){
+                max =Math.max(max, map.get(s)+1);
+            }
         }
-      return pointsOnLine;  
+        return max;
     }
     
-    public String getSlope(int[] a, int [] b){
-        int x =a[0] - b[0];
-        int y =a[1] - b[1];
-        int gcd =getGcd(x,y);
-        x = (gcd ==0 ? x /Math.max(x,y) : x/gcd);
-        y = (gcd ==0 ? y /Math.max(x,y) : y/gcd);
+    public String getSlope(int a[], int b[]){
+        int x = a[0] - b[0];
+        int y = a[1] - b[1];
+        int gcd = getGCD(x,y); 
+        x = (gcd ==0 ? x/Math.max(x,y) : x/gcd);
+        y = (gcd ==0 ? y/Math.max(x,y) : y/gcd);
         
-        return ""+ x +"_"+y;
+        return "" +x +"_" +y;
     }
     
-    public int getGcd(int x, int y){
-        
-        return y==0 ? x :getGcd(y, x % y);
+    public int getGCD(int x ,int y){
+        return y==0 ? x : getGCD(y,x % y) ;
     }
-    
 }
