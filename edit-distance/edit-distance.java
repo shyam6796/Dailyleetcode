0a3404c37c19bdcb433195d3e371cc6fc904594d
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2022-01-01 16:15:24 -0700

    Time: 5 ms (61.85%), Space: 39 MB (73.64%) - LeetHub

--- edit-distance/edit-distance.java
+++ edit-distance/edit-distance.java
@@ -10,14 +10,15 @@ class Solution {
                     dp[i][j]=j;
                 }else if(j==0){
                     dp[i][j]=i;
-                }else if(word1.charAt(i-1) ==word2.charAt(j-1)){
-                    dp[i][j] =dp[i-1][j-1];
                 }else{
-                    dp[i][j] =1+ Math.min(dp[i-1][j-1], Math.min(dp[i-1][j],dp[i][j-1]));
+                    if(word1.charAt(i-1)==word2.charAt(j-1)){
+                        dp[i][j]= dp[i-1][j-1];
+                    }else{
+                        dp[i][j]=1+ Math.min(dp[i-1][j-1], Math.min(dp[i-1][j],dp[i][j-1]));
+                    }
                 }
-                
             }
         }
         return dp[m][n];
-    }
+    }    
 }
