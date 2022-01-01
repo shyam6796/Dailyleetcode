5c2cf0bd41b345a73ffb16d6ebb729a9aea47875
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2021-12-31 21:49:33 -0700

    Time: 5 ms (16.29%), Space: 39.7 MB (20.33%) - LeetHub

--- valid-number/valid-number.java
+++ valid-number/valid-number.java
@@ -1,25 +1,28 @@
 class Solution {
     public boolean isNumber(String s) {
-     s = s.trim();
-        boolean eSeen = false;
-        boolean numSeen = false;
+        boolean numSeen =false;
+        boolean eSeen =false;
         boolean dotSeen = false;
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (Character.isDigit(c)) {
-                numSeen = true;
-            } else if (c == 'e'  || c == 'E') {
-                if (eSeen || !numSeen) return false;
-                eSeen = true;
-                numSeen = false;
-            } else if (c == '.') {
-                if (eSeen || dotSeen) return false;
-                dotSeen = true;
-            } else if (c == '-' || c == '+') {
-                if (i != 0 && s.charAt(i - 1) != 'e' && s.charAt(i - 1) != 'E') return false;
-            } else return false;
-        }
         
+        for(int i=0; i<s.length(); i++){
+            char c =s.charAt(i);
+            if(Character.isDigit(c)){
+                numSeen =true;
+            }else if(c=='e' || c=='E'){
+                if(eSeen || !numSeen)return false;
+                else{
+                    eSeen = true;
+                    numSeen = false;
+                }
+            }else if(c=='.'){
+                if(dotSeen || eSeen)return false;
+                else dotSeen =true;
+            }else if(c=='+' || c=='-'){
+                if(i !=0 && s.charAt(i-1) !='E' && s.charAt(i-1) != 'e' ){
+                    return false;
+                }
+            }else return false;
+        }
         return numSeen;
     }
 }
