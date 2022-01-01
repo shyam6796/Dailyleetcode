fe742dfa3ee6459aca35c2a140cf079e9289a9e1
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2022-01-01 14:15:30 -0700

    Time: 38 ms (8.39%), Space: 39.7 MB (49.22%) - LeetHub

--- minimum-insertions-to-balance-a-parentheses-string/minimum-insertions-to-balance-a-parentheses-string.java
+++ minimum-insertions-to-balance-a-parentheses-string/minimum-insertions-to-balance-a-parentheses-string.java
@@ -1,54 +1,48 @@
 class Solution {
     public int minInsertions(String s) {
-             Stack<Character> stack = new Stack<>();
-            int insertionsNeeded = 0;
-            for (int i = 0; i < s.length(); i++) {
-                char c = s.charAt(i);
-                if (c == '(') {
-                    if (stack.isEmpty()) {
-                        stack.add(c);
-                    } else {
-                        if (stack.peek() == ')') {
-                          
-                            insertionsNeeded++;
-                            stack.pop();
-                            stack.pop();
-                            stack.add(c);
-                        } else {
-                            stack.add(c);
-                        }
-                    }
-                } else if (c == ')') {
-                    if (stack.isEmpty()) {
-                  
-                        insertionsNeeded++;
-                        stack.add('(');
-                        stack.add(c);
-                    } else {
-                        if (stack.peek() == ')') {
-                        
-                            stack.pop();
-                            stack.pop();
-                        } else {
-                            stack.add(c);
-                        }
-                    }
-                }
-            }
-            if (stack.isEmpty()) {
-                return insertionsNeeded;
-            } else {
-                while (!stack.isEmpty()) {
-                    char pop = stack.pop();
-                    if (pop == '(') {
-                        insertionsNeeded += 2;
-                    } else {
-                        insertionsNeeded++;
-                        stack.pop();
-                    }
-                }
-                return insertionsNeeded;
-            }
+        Stack<Character> st = new Stack();
+        int insert =0;
         
+        for(char c :s.toCharArray()){
+            if(c == '('){
+                if(st.isEmpty()){
+                    st.push(c);
+                }else if(st.peek() == ')'){
+                    insert++;
+                    st.pop();
+                    st.pop();
+                    st.push(c);
+                }else{
+                    st.push(c);
+                }
+            }else if(c ==')'){
+                if(st.isEmpty()){
+                    insert++;
+                    st.push('(');
+                    st.push(')');
+                }else if(st.peek() ==')'){
+                    st.pop();
+                    st.pop();
+                }else {
+                    st.push(c);
+                }
+            }
+            
+        }
+        
+        if(st.isEmpty()) return insert;
+        
+        while(!st.isEmpty()){
+            char c= st.pop();
+            if(c=='('){
+                insert+=2;
+                
+            }else{
+                insert++;
+                st.pop();
+               
+            }
+        }
+        return insert;
     }
 }
