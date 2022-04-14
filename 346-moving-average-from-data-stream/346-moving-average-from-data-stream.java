e323707cafe09ece40217f97a112907734a9dcd5
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2022-04-13 23:40:35 -0700

    Time: 72 ms (46.56%), Space: 47 MB (82.01%) - LeetHub

---
+++ 346-moving-average-from-data-stream/346-moving-average-from-data-stream.java
@@ -0,0 +1,32 @@
+class MovingAverage {
+    List<Integer>list= new ArrayList<>();
+    int n;
+    
+    public MovingAverage(int size) {
+        n =size;
+    }
+    
+    public double next(int val) {
+         list.add(0,val);
+        int temp =0;
+        if(list.size()<n){ 
+             for(int i :list){
+                temp +=i;
+            }
+            return (double)temp/list.size();
+            
+        }else{
+            for(int i=0; i<n; i++){
+                temp +=list.get(i);
+            }
+            return (double)temp/n;
+        }
+        
+    }
+}
+
+/**
+ * Your MovingAverage object will be instantiated and called as such:
+ * MovingAverage obj = new MovingAverage(size);
+ * double param_1 = obj.next(val);
+ */
