6f6410113e11e30ad5b138d04ccef842b38c8ab5
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2022-04-16 20:28:45 -0700

    Time: 1 ms (80.11%), Space: 41.7 MB (79.50%) - LeetHub

--- 50-powx-n/50-powx-n.java
+++ 50-powx-n/50-powx-n.java
@@ -1,20 +1,19 @@
 class Solution {
     public double myPow(double x, int n) {
         long N = n;
-        if(n==0){
-            return 1;
-        }
+        if(n==0)return 1;
         if(n<0){
+            N=-N;
             x=1/x;
-           N=-N;
         }
-       double ans = 1;
-       double currProduct =x; 
-        for(long i = N; i > 0; i = i/2){
-            if(i%2==1){
-                ans =ans*currProduct;
+        double ans=1;
+        double currentProduct =x;
+        
+        for(long i=N; i>0; i=i/2){
+            if(i%2 ==1){
+                ans= ans*currentProduct;
             }
-                currProduct =currProduct*currProduct;
+            currentProduct=currentProduct * currentProduct;
         }
         return ans;
     }
