346120ccc8deec5dede72a1bc378c205f18e98c6
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2022-04-17 00:12:43 -0700

    Time: 3 ms (98.93%), Space: 50.4 MB (94.94%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -1,47 +1,44 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-         return quickSelect(points,k);
+        return quickSelect(points,k);
     }
     
-    public int[][] quickSelect(int[][] points, int k){
-        int pivot=points.length;
-        int left =0;
-        int right =points.length-1;
+    public int[][] quickSelect(int[][] points, int k) {
+        int left=0;
+        int right=points.length-1;
+        int pivot =points.length;
         while(pivot !=k){
-            pivot = partiotion(points,left,right);
+            pivot =partiotion(points,left,right);
             if(pivot >k){
                 right =pivot-1;
             }else{
                 left =pivot;
             }
         }
+        
         return Arrays.copyOf(points,k);
     }
     
-    public int partiotion(int[][] points,int left, int right){
-        int [] pivot =choosePivot(points, left, right);
-        int pivotDist = dist(pivot);
-        while(left <right){
-            if(pivotDist <= dist(points[left])){
-                int temp [] =points[right];
-                points[right] =points[left];
-                points[left] =temp;
+    public int partiotion(int points[][],  int left, int right){
+        int pivot[] =points[left + (right - left) / 2];
+        int pivot_dist= dist(pivot);
+        while(left<right){
+            if(pivot_dist <= dist(points[left])){
+                int temp[] =points[left];
+                points[left]=points[right];
+                points[right]=temp;
                 right--;
             }else{
                 left++;
             }
         }
-        if (dist(points[left]) < pivotDist)
+        if (dist(points[left]) < pivot_dist)
             left++;
         
         return left;
     }
     
-    public int [] choosePivot(int[][] points, int left, int right){
-        return points[left + (right - left) / 2];
-    }
-    
-    public int dist(int [] a){
-        return a[0]*a[0] +a[1] *a[1];
+    public int dist(int a[]){
+        return a[0]*a[0] +a[1]*a[1];
     }
 }
