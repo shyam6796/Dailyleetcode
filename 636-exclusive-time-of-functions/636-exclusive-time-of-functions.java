043cf6e9f02f783d53b5fd28092b4bf09dd4286b
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2022-04-17 18:44:55 -0700

    Time: 11 ms (83.91%), Space: 42.7 MB (95.36%) - LeetHub

--- 636-exclusive-time-of-functions/636-exclusive-time-of-functions.java
+++ 636-exclusive-time-of-functions/636-exclusive-time-of-functions.java
@@ -1,28 +1,26 @@
 class Solution {
     public int[] exclusiveTime(int n, List<String> logs) {
         int [] ans = new int[n];
-        Stack<Integer> stack =new Stack<>();
-        String [] log =logs.get(0).split(":");
+        Stack<Integer> stack = new Stack<>();
+        String log[] =logs.get(0).split(":");
+        int time =Integer.valueOf(log[2]);
+        int last=time;
         int id =Integer.valueOf(log[0]);
-        int timeStamp = Integer.valueOf(log[2]);
-        int lastTime =timeStamp;
-        String s =log[1];
         stack.add(id);
         
         for(int i=1; i<logs.size(); i++){
-            log = logs.get(i).split(":");
+            log =logs.get(i).split(":");
+            time =Integer.valueOf(log[2]);
             id =Integer.valueOf(log[0]);
-            timeStamp =Integer.valueOf(log[2]);
-            s =log[1];
-            if(s.equals("start")){
-                if(!stack.isEmpty())ans[stack.peek()] += timeStamp-lastTime;
-                lastTime =timeStamp;
+            
+            if(log[1].equals("start")){
+                if(!stack.isEmpty())ans[stack.peek()] += time -last;
+                last=time;
                 stack.push(id);
             }else{
-               ans[stack.pop()] += timeStamp-lastTime+1;
-                lastTime =timeStamp+1;
+                if(!stack.isEmpty())ans[stack.pop()] += time-last+1;
+                last =time+1;
             }
-            
         }
         return ans;
     }
