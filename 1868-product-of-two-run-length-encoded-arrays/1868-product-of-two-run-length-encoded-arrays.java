4cea5d79889e1e3589095b42802cf055ac63adbe
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2022-04-18 22:09:00 -0700

    Time: 44 ms (39.25%), Space: 234.8 MB (24.73%) - LeetHub

--- 1868-product-of-two-run-length-encoded-arrays/1868-product-of-two-run-length-encoded-arrays.java
+++ 1868-product-of-two-run-length-encoded-arrays/1868-product-of-two-run-length-encoded-arrays.java
@@ -1,33 +1,29 @@
 class Solution {
     public List<List<Integer>> findRLEArray(int[][] encoded1, int[][] encoded2) {
-        List<List<Integer>> result = new ArrayList<>();
+        List<List<Integer>> list =new ArrayList<>();
+        int i=0, j=0;
         int m =encoded1.length;
         int n =encoded2.length;
-        int i=0,j=0;
-        
-        while(i<m && j<n){
+        while(i <m && j <n){
             int product = encoded1[i][0] *encoded2[j][0];
             int freq = Math.min(encoded1[i][1],encoded2[j][1]);
             
-            if(result.isEmpty() || result.get(result.size()-1).get(0) != product){
-                List<Integer> list =new ArrayList<>();
-                list.add(product);
-                list.add(freq);
-                result.add(list);
+            if(list.isEmpty() || list.get(list.size()-1).get(0) !=product){
+                List<Integer> ans =new ArrayList<>();
+                ans.add(product);
+                ans.add(freq);
+                list.add(ans);
             }else{
-               List<Integer> list =result.get(result.size()-1);
-                list.set(1,list.get(1)+freq);
-                result.set(result.size()-1,list);
+                List<Integer> temp =list.get(list.size()-1);
+                temp.set(1,temp.get(1) +freq);
+                list.set(list.size()-1, temp);
             }
-            
             encoded1[i][1] -=freq;
             encoded2[j][1] -=freq;
-            
-            if(encoded1[i][1] ==0)i++;
-            if(encoded2[j][1]==0)j++;
-            
+            if( encoded1[i][1] ==0) i++;
+            if( encoded2[j][1] ==0) j++;
         }
-        return result;
         
+        return list;
     }
 }
