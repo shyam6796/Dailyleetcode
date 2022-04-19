6864fec578901b3df124a49c42e91e85e97fa546
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2022-04-18 21:59:32 -0700

    Time: 11 ms (8.44%), Space: 42.9 MB (17.17%) - LeetHub

--- 140-word-break-ii/140-word-break-ii.java
+++ 140-word-break-ii/140-word-break-ii.java
@@ -1,24 +1,22 @@
 class Solution {
-    Map<String,List<String>> map = new HashMap<>();
+    Map<String,List<String>> map =new HashMap<>();
     public List<String> wordBreak(String s, List<String> wordDict) {
-        List<String> list = new ArrayList<>();
-        
+        List<String> list =new ArrayList<>();
         if(s.equals("")){
             list.add("");
             return list;
         }
-         if(map.containsKey(s)){
+        if(map.containsKey(s)){
             return map.get(s);
         }
         
-        
-        for(String word :wordDict){
+        for(String word : wordDict){
             if(s.startsWith(word)){
-                String subWord =s.substring(word.length());
-                List<String> subList =wordBreak(subWord, wordDict);
-                for(String temp :subList){
-                    String space = temp.equals("") ? "" :" ";
-                    list.add(word +space+temp);
+                String subWord = s.substring(word.length());
+                List<String> subList = wordBreak(subWord,wordDict);
+                for(String temp : subList){
+                    String space = temp.length()==0 ? "" : " ";
+                    list.add(word + space+temp);
                 }
             }
         }
