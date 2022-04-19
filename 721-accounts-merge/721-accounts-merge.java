89a131a93e262091e0b32205ab91987ca7c43d75
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2022-04-18 18:43:22 -0700

    Time: 36 ms (84.99%), Space: 48.7 MB (84.79%) - LeetHub

--- 721-accounts-merge/721-accounts-merge.java
+++ 721-accounts-merge/721-accounts-merge.java
@@ -1,47 +1,48 @@
 class Solution {
     Map<String,List<String>> map = new HashMap<>();
-    Set<String> visited =new HashSet<>();
+    Set<String> visited = new HashSet<>();
     public List<List<String>> accountsMerge(List<List<String>> accounts) {
-        
-        List<List<String>> mergeAccounts = new ArrayList<>();
-        
-        for(List<String> list : accounts){
-            String firstEmail =list.get(1);
-            if(!map.containsKey(firstEmail)){
-                map.put(firstEmail,new ArrayList<>());
+        for(List<String> account : accounts){
+            String firstemail = account.get(1);
+            if(!map.containsKey(firstemail)){
+                map.put(firstemail,new ArrayList<>());
             }
-          for(int j =2; j<list.size(); j++){
-                String email =list.get(j);
+           
+            for(int i=2; i<account.size(); i++){
+                String email =account.get(i);
                 
-                map.get(firstEmail).add(email);
+                map.get(firstemail).add(email);
                 if(!map.containsKey(email)){
                     map.put(email, new ArrayList<>());
                 }
-                map.get(email).add(firstEmail);
+                map.get(email).add(firstemail);
             }
         }
         
-        for(List<String> list : accounts){
-            List<String> mergeAccount = new ArrayList<>();
-            mergeAccount.add(list.get(0));
-            String firstEmail =list.get(1);
-           if(!visited.contains(firstEmail)){ 
-                helper(mergeAccount,firstEmail);
+        List<List<String>> list = new ArrayList<>();
+        
+        for(List<String> account :accounts){
+             List<String> mergeAccount = new ArrayList<>();
+            mergeAccount.add(account.get(0));
+            String firstEmail =account.get(1);
+            if(!visited.contains(firstEmail)){
+                 helepr(mergeAccount,firstEmail);
                 Collections.sort(mergeAccount.subList(1,mergeAccount.size()));
-                mergeAccounts.add(mergeAccount);        
+                list.add(mergeAccount);
             }
         }
-        return mergeAccounts;
+        
+        return list;
     }
-    public void helper(List<String>mergeAccount, String email){
+    
+    public void helepr(List<String> mergeAccount, String email){
         mergeAccount.add(email);
-         visited.add(email);
-        if(!map.containsKey(email)){
-            return ;
-        }
-        for(String neighbor : map.get(email)){
-            if (!visited.contains(neighbor)) {
-                helper(mergeAccount, neighbor);
+       visited.add(email);
+        if(!map.containsKey(email)) return;
+        
+        for(String s : map.get(email)){
+            if(!visited.contains(s)){
+                 helepr(mergeAccount,s);
             }
         }
     }
