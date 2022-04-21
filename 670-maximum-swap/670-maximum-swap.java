e428ffb579d235c84b8b4dcd6b8068ef3e86c032
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2022-04-20 18:41:23 -0700

    Time: 1 ms (78.92%), Space: 41.1 MB (42.16%) - LeetHub

--- 670-maximum-swap/670-maximum-swap.java
+++ 670-maximum-swap/670-maximum-swap.java
@@ -1,20 +1,22 @@
 class Solution {
     public int maximumSwap(int num) {
-        int []map =new int[10];
-        char[] arr = String.valueOf(num).toCharArray();
-        for(int i =0; i<arr.length; i++){
+        char [] arr =String.valueOf(num).toCharArray();
+        int [] map =new int[10];
+        
+        for(int i=0; i<arr.length; i++){
             map[arr[i] -'0']=i;
         }
         
-        for(int i =0; i<arr.length; i++){
-            int digit= arr[i] -'0';
-            for(int j=9; j >digit; j--){
-                if(map[j]> i){
-                    char c =arr[i];
-                    arr[i] =arr[map[j]];
-                    arr[map[j]] =c;
+        for(int i=0; i<arr.length; i++){
+            int number =arr[i] -'0';
+            for(int j=9; j>number; j--){
+                if(map[j] >i){
+                    char temp =arr[map[j]];
+                    arr[map[j]]=arr[i];
+                    arr[i] =temp;
                     
-                     return Integer.valueOf(new String(arr));
+                    return Integer.valueOf(new String(arr));
+                    
                 }
             }
         }
