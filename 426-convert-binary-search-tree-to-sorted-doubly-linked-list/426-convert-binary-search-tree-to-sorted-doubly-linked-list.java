7b4bc71f00e6c2a8a933842112a837a6b034cb0a
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2022-04-29 01:00:42 -0700

    Time: 0 ms (100.00%), Space: 42.5 MB (51.14%) - LeetHub

--- 426-convert-binary-search-tree-to-sorted-doubly-linked-list/426-convert-binary-search-tree-to-sorted-doubly-linked-list.java
+++ 426-convert-binary-search-tree-to-sorted-doubly-linked-list/426-convert-binary-search-tree-to-sorted-doubly-linked-list.java
@@ -20,28 +20,26 @@ class Node {
 */
 
 class Solution {
-    Node last;
     Node first;
+    Node last;
     public Node treeToDoublyList(Node root) {
         if(root==null)return root;
         helper(root);
-        first.left =last;
+        first.left=last;
         last.right=first;
         return first;
     }
     
     public void helper(Node root){
-        if(root==null){
-            return;
-        }
-        helper(root.left);
+        if(root.left !=null) helper(root.left);
+        
         if(last ==null){
-            first=root;
+            first= root;
         }else{
             root.left=last;
-            last.right=root;
+            last.right= root;
         }
-        last=root;
-        helper(root.right);
+        last =root;
+         if(root.right !=null) helper(root.right);
     }
 }
