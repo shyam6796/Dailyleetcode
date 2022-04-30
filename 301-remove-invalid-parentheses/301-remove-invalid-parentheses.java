60ed08a4ddb76b6966139d4a72be33f9f08a61c6
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2022-04-29 23:51:24 -0700

    Time: 730 ms (5.01%), Space: 50.6 MB (33.00%) - LeetHub

--- 301-remove-invalid-parentheses/301-remove-invalid-parentheses.java
+++ 301-remove-invalid-parentheses/301-remove-invalid-parentheses.java
@@ -1,43 +1,37 @@
 class Solution {
     Set<String> set = new HashSet<>();
     public List<String> removeInvalidParentheses(String s) {
-        List<String> list = new ArrayList<>();
-        int minRemove =helper(s);
-       // System.out.println(minRemove);
-        helper2(list,minRemove,s);
+        List<String> list =new ArrayList<>();
+        int numOfRemoval =helper(s);
+        helper2(list,numOfRemoval,s);
         return list;
     }
-     public  void  helper2(List<String> list, int minRemove, String s){
-         if(set.contains(s))return;
-         set.add(s);
-         if(minRemove==0){
-            if(helper(s)==0)list.add(s);
-            return;
-        }
-         for(int i=0; i<s.length(); i++){
-            String left =s.substring(0,i);
-            String right =s.substring(i+1);
-            
-             helper2(list,minRemove-1, left+right);
-        }
-     }
     
-    public  int helper(String s){
-        Stack<Character> stack = new Stack<>();
-        for(int i=0; i<s.length(); i++){
-            if(stack.isEmpty()){
-                if(s.charAt(i) =='(' || s.charAt(i) ==')')stack.push(s.charAt(i));
-                continue;
-            }else{
-                if(s.charAt(i) =='('){
-                    stack.push(s.charAt(i));
-                }else if(s.charAt(i) ==')' && stack.peek()=='('){
-                        stack.pop();
-                }else if(s.charAt(i) ==')'){
-                    stack.add(s.charAt(i));
-                }
+    public int helper(String s){
+        Stack<Character> stack =new Stack();
+        for(char c :s.toCharArray()){
+            if(c=='('){
+                stack.add(c);
+            }else if(c==')'){
+                if(!stack.isEmpty() && stack.peek() =='(') stack.pop();
+                else stack.push(c);
             }
         }
+        
         return stack.size();
-    } 
+    }
+    
+    public void helper2(List<String> list, int k, String s){
+        if(set.contains(s))return;
+        set.add(s);
+        if(k==0 && helper(s) ==0){
+            list.add(s);
+            return;
+        }
+        for(int i=0; i<s.length(); i++){
+            String left =s.substring(0,i);
+            String right=s.substring(i+1);
+            helper2(list,k-1,left+right);
+        }
+    }
 }
