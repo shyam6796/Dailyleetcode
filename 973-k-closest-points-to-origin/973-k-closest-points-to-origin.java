f34c5990e723fe04ec999e2a7e5e7f0a59658b56
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2022-04-30 23:06:23 -0700

    Time: 4 ms (97.28%), Space: 114.5 MB (58.80%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -1,27 +1,28 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        return quickSelect(points,k);
+        return quickSelct(points, k);
     }
     
-    public int[][] quickSelect(int[][] points, int k) {
+    public int[][] quickSelct(int[][] points, int k){
         int left=0;
         int right=points.length-1;
-        int pivot =points.length;
-        while(pivot !=k){
-            pivot =partiotion(points,left,right);
-            if(pivot >k){
-                right =pivot-1;
+        int pivot_index =right+1;
+        
+        while(k !=pivot_index){
+            pivot_index =partiton(points,left,right);
+            
+            if(pivot_index >k){
+                right =pivot_index-1;
             }else{
-                left =pivot;
+                left =pivot_index;
             }
         }
-        
         return Arrays.copyOf(points,k);
     }
     
-    public int partiotion(int points[][],  int left, int right){
-        int pivot[] =points[left + (right - left) / 2];
-        int pivot_dist= dist(pivot);
+    public int partiton(int[][] points, int left, int right){
+        int pivot[] = points[left +(right-left)/2];
+        int pivot_dist =dist(pivot);
         while(left<right){
             if(pivot_dist <= dist(points[left])){
                 int temp[] =points[left];
@@ -36,6 +37,7 @@ class Solution {
             left++;
         
         return left;
+        
     }
     
     public int dist(int a[]){
