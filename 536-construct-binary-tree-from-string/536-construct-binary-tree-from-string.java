a1823eb058684b113b652ceff16dcde1c880db15
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2022-05-03 00:37:39 -0700

    Time: 4 ms (86.69%), Space: 42.8 MB (79.71%) - LeetHub

---
+++ 536-construct-binary-tree-from-string/536-construct-binary-tree-from-string.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+     int index=0;
+    public TreeNode str2tree(String s) {
+        return helper(s);
+    }
+   
+    public TreeNode helper(String s){
+        if(index>=s.length())return null;
+        
+        if(s.charAt(index) ==')'){
+            index++;
+            return null;
+        }
+        
+         
+        int value =getValue(s);
+        TreeNode root =new TreeNode(value);
+        
+         if(index <s.length() && s.charAt(index)=='('){
+            index++;
+            root.left = helper(s);
+            
+        }
+        if(index <s.length() && s.charAt(index)=='('){
+            index++;
+            root.right = helper(s);
+            
+        }
+        index++;
+        
+        return root;
+    }
+    
+    public int getValue(String s){
+        StringBuilder sb = new StringBuilder();
+        while(index <s.length() && s.charAt(index) != '(' &&s.charAt(index) != ')'){
+            sb.append(s.charAt(index++));
+        }
+        return Integer.valueOf(sb.toString());
+    }
+}
