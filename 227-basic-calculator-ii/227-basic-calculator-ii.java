5ad75ba3addf87163e7c053840e5878d5c45f4b5
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2022-05-09 23:13:30 -0700

    Time: 24 ms (37.30%), Space: 44.4 MB (54.35%) - LeetHub

--- 227-basic-calculator-ii/227-basic-calculator-ii.java
+++ 227-basic-calculator-ii/227-basic-calculator-ii.java
@@ -1,44 +1,43 @@
 class Solution {
-    int index =0;
+    int index=0;
     public int calculate(String s) {
-        Stack<Integer> stack = new Stack<>();
+        Stack<Integer> stack =new Stack<>();
         int num=0;
         char sign ='+';
         
         while(index <=s.length()){
-            char c = s.length() == index ? '#' : s.charAt(index);
-            if(c == ' '){
+            char c =index ==s.length() ? '#' : s.charAt(index);
+            if(c==' '){
                 index++;
                 continue;
             }
             if(Character.isDigit(c)){
-                num =num *10 + c-'0';
+                num =num*10+ c -'0';
                 index++;
             }else{
-                pushToStack(stack,sign,num);
+                pushStack(num,stack,sign);
                 num=0;
                 sign=c;
                 index++;
             }
-           
         }
-         return getResult(stack);
+        
+        return getResult(stack);
     }
     
-    public int getResult(Stack<Integer> stack){
-        int ans=0;
-        while(!stack.isEmpty()){
-            ans += stack.pop();
-        }
-        return ans;
-    }
-    
-    public void pushToStack(Stack<Integer> stack, char sign, int num){
+    public void pushStack(int num, Stack<Integer> stack, char sign){
         switch(sign){
             case '+' : stack.push(num); break;
             case '-' : stack.push(-num); break;
             case '*' : stack.push(num * stack.pop()); break;
             case '/' : stack.push(stack.pop()/num); break;
         }
+    }
+    public int getResult(Stack<Integer> stack){
+        int ans=0;
+        while(!stack.isEmpty()){
+            ans += stack.pop();
+        }
+        return ans;
     }
 }
