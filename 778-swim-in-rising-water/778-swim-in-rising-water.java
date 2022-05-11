f7ff4c04b7af598d50bad7c88ab1139657bbc186
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2022-05-11 00:52:05 -0700

    Time: 13 ms (66.00%), Space: 45.6 MB (42.20%) - LeetHub

--- 778-swim-in-rising-water/778-swim-in-rising-water.java
+++ 778-swim-in-rising-water/778-swim-in-rising-water.java
@@ -1,45 +1,42 @@
 class Solution {
-    int[] dx = {1 , 0 , -1 , 0};
-    int[] dy = {0 ,1 , 0 , -1};
+    int [][] dirs ={{0,1},{1,0},{0,-1},{-1,0}};
     public int swimInWater(int[][] grid) {
-        int n = grid.length;
-        int[][] dis = new int[n][n];
-        
-        for(int i = 0 ; i< n ; i++){
-            Arrays.fill(dis[i] , Integer.MAX_VALUE);
-        }
-        
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) ->{
+         
+           
+          
+        int m =grid.length;
+        int n=grid[0].length;
+        int dist[][] = new int[m][n];
+        PriorityQueue<int[]> queue =new PriorityQueue<>((a,b) ->{
             return Integer.compare(a[2],b[2]);
         });
-        
-        pq.add(new int[]{0 , 0 ,grid[0][0]});
-        
-        while(!pq.isEmpty()){
-            int[] arr = pq.poll();
-            int x = arr[0] , y = arr[1] , time = arr[2];
-            for(int i = 0; i< 4 ; i++){
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                
-                if(nx < 0 || nx >= n || ny < 0 || ny >= n){
-                    continue;
-                }
-                
-                int rem = Math.max(0 , grid[nx][ny] - time);
-                
-                if(rem + time < dis[nx][ny]){
-                    dis[nx][ny] = rem + time;
-                    pq.add(new int[]{nx , ny , dis[nx][ny]});
-                }
-    
-            }            
+        queue.add(new int[]{0,0,grid[0][0]});
+        for(int i=0; i<m; i++){
+            Arrays.fill(dist[i],Integer.MAX_VALUE);
         }
-  
         
+        while(!queue.isEmpty()){
+            int [] curr =queue.poll();
+            int x = curr[0];
+            int y = curr[1];
+            int time =curr[2];
+            
+            for(int dir[] : dirs){
+               
+                int nx =x+dir[0];
+                int ny =y+dir[1];
+                
+                if(nx <0 || nx >=m ||ny <0 || ny >=n)continue;
+                
+                int rem = Math.max(0,grid[nx][ny]-time);
+                
+                if(rem +time <dist[nx][ny]){
+                    dist[nx][ny]=rem+time;
+                    queue.add(new int[]{nx,ny,dist[nx][ny]});
+                }
+            }
+        }
         
-        return dis[n-1][n-1] == Integer.MAX_VALUE ? 0 : dis[n-1][n-1] ;
-        
-        
+        return dist[m-1][n-1] ==Integer.MAX_VALUE ? 0 : dist[m-1][n-1];
     }
 }
