61605502a009498a3486b3d00b5c0c82f5ccc658
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2022-05-10 22:16:26 -0700

    Time: 2 ms (98.20%), Space: 57.5 MB (34.09%) - LeetHub

--- 825-friends-of-appropriate-ages/825-friends-of-appropriate-ages.java
+++ 825-friends-of-appropriate-ages/825-friends-of-appropriate-ages.java
@@ -1,18 +1,23 @@
 class Solution {
     public int numFriendRequests(int[] ages) {
-        int arr[] =new int[121];
-        for(int age :ages){
-            arr[age]++;
+        int age[] =new int[121];
+        
+        for(int i=0; i<ages.length; i++){
+            age[ages[i]]++;
         }
-        int count =0;
+        
+        
+        int ans=0;
         
         for(int a =0; a<=120; a++){
-            for(int b =(int)(0.5*a)+8; b<=a; b++){
-                count += arr[a]*arr[b];
-               if(a==b) count -=arr[a];
+            for(int b = (int) (0.5 *a) +8;  b<=a; b++){
+                ans += age[b]*age[a];
+            if(b==a){
+                ans -=age[a];
+            }
             }
         }
         
-        return count;
+        return ans;
     }
 }
