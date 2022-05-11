22a027c56634469ce532f1c9dd81c18620814141
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2022-05-11 01:21:22 -0700

    Time: 10 ms (59.20%), Space: 50.5 MB (17.63%) - LeetHub

--- 536-construct-binary-tree-from-string/536-construct-binary-tree-from-string.java
+++ 536-construct-binary-tree-from-string/536-construct-binary-tree-from-string.java
@@ -14,42 +14,40 @@
  * }
  */
 class Solution {
-     int index=0;
+    int index=0;
     public TreeNode str2tree(String s) {
         return helper(s);
     }
-   
+    
     public TreeNode helper(String s){
-        if(index>=s.length())return null;
+        if(index >= s.length())return null;
         
         if(s.charAt(index) ==')'){
             index++;
             return null;
         }
         
-         
-        int value =getValue(s);
-        TreeNode root =new TreeNode(value);
-        
-         if(index <s.length() && s.charAt(index)=='('){
+        int val =getValue(s);
+        TreeNode root = new TreeNode(val);
+        if(index <s.length() && s.charAt(index) =='('){
             index++;
-            root.left = helper(s);
-            
+            root.left=helper(s);
         }
-        if(index <s.length() && s.charAt(index)=='('){
+         if( index <s.length() && s.charAt(index) =='('){
             index++;
-            root.right = helper(s);
-            
+            root.right=helper(s);
         }
         index++;
-        
         return root;
+        
     }
     
     public int getValue(String s){
-        StringBuilder sb = new StringBuilder();
-        while(index <s.length() && s.charAt(index) != '(' &&s.charAt(index) != ')'){
-            sb.append(s.charAt(index++));
+        StringBuilder sb =new StringBuilder();
+        
+        while(index <s.length() && s.charAt(index) !='(' && s.charAt(index) !=')'){
+            sb.append(s.charAt(index));
+            index++;
         }
         return Integer.valueOf(sb.toString());
     }
