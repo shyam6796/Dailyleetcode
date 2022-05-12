1efd75d5dff8f985ac4f57953abc00053bf2832a
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2022-05-11 19:05:33 -0700

    Time: 11 ms (85.00%), Space: 42.5 MB (76.07%) - LeetHub

--- 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.java
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.java
@@ -8,57 +8,54 @@
  * }
  */
 class Solution {
+    Map<TreeNode,TreeNode> map =new HashMap<>();
+    
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
-        List<Integer> list = new ArrayList<>();        
-        Map<TreeNode,TreeNode> map =new HashMap<>();
-        
-        map.put(root,null);
-        mapBuilder(root,map);
-       
-        
+        List<Integer>list =new ArrayList<>();
+        buildMap(root,null);  
         Queue<TreeNode> queue =new LinkedList<>();
-        Set<TreeNode> set= new HashSet<>();
-        set.add(target);
-        queue.add(target);
+        Set<TreeNode> set =new HashSet<>();
         
+        queue.add(target);
+        set.add(target);
         int lvl=0;
+        
         while(!queue.isEmpty()){
             int size =queue.size();
+           
             for(int i=0; i<size; i++){
                 TreeNode curr=queue.poll();
-                if(lvl ==k){
+                if(lvl==k){
                     list.add(curr.val);
+                    continue;
                 }
-                if(curr.left != null && !set.contains(curr.left)){
-                    queue.add(curr.left);
-                     set.add(curr.left);
+                if(map.get(curr) !=null && !set.contains(map.get(curr)) ){
+                        queue.add(map.get(curr));
+                        set.add(map.get(curr));
                 }
-                if(curr.right != null && !set.contains(curr.right)){
-                    queue.add(curr.right);
-                     set.add(curr.right);
+                 if(curr.left !=null && !set.contains(curr.left)){
+                        queue.add(curr.left);
+                       set.add(curr.left);
                 }
-                if(map.get(curr) !=null && !set.contains(map.get(curr))){
-                    queue.add(map.get(curr));
-                     set.add(map.get(curr));
+                if(curr.right !=null && !set.contains(curr.right)){
+                        queue.add(curr.right);
+                        set.add(curr.right);
                 }
-                
+            
             }
             lvl++;
-            if(lvl >k) break;
-        }
+            if(lvl >k)break;
+        }      
+              
+    return list;    
+    
         
-        //System.out.println(map);
-        return list;
-
     }
-    public void mapBuilder(TreeNode root,Map<TreeNode,TreeNode> map){
-        if(root.left !=null){
-            map.put(root.left,root);
-            mapBuilder(root.left,map);
-        }
-        if(root.right !=null){
-            map.put(root.right,root);
-            mapBuilder(root.right,map);
-        }
+    
+    public void buildMap(TreeNode root, TreeNode parent){
+        if(root==null)return;
+        map.put(root,parent);
+        buildMap(root.left,root);
+        buildMap(root.right,root);
     }
 }
