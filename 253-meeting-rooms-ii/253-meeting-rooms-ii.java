1d7db2585fb6ea3091059d7f36f589935dbf6798
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2022-05-11 19:42:48 -0700

    Time: 9 ms (60.84%), Space: 45.2 MB (49.84%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.java
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.java
@@ -1,22 +1,20 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-       if(intervals ==null || intervals.length==0)return 0;
+        if(intervals ==null || intervals.length==0)return 0;
         
-        Arrays.sort(intervals, (a,b) -> a[0] -b[0]);
-        PriorityQueue<int[]>  PQ = new PriorityQueue<int[]>((a,b) -> a[1] -b[1]);
-        PQ.add(intervals[0]);
-       // System.out.println(PQ);
+        Arrays.sort(intervals, (a,b) -> a[0] -b[0]); 
+        PriorityQueue<int[]> queue =new PriorityQueue<>((a,b) -> a[1]-b[1]);
+        queue.add(intervals[0]);
+        
         for(int i=1; i<intervals.length; i++){
-            int[] current =PQ.poll();
-            int[] next=intervals[i];
-            if(current[1] <=next[0]){
-                current[1]=next[1];
+            int [] curr= queue.poll();
+            if(curr[1]  <= intervals[i][0]){
+                curr[1] =intervals[i][1];
             }else{
-                PQ.add(next);
+                queue.add(intervals[i]);
             }
-            PQ.add(current);
-            //System.out.println(PQ);
+            queue.add(curr);
         }
-        return PQ.size();
+        return queue.size();
     }
 }
