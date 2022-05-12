66f24a95186352d69b9c1b8f28996075f02a4852
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2022-05-11 21:02:23 -0700

    Time: 95 ms (7.25%), Space: 101.5 MB (32.42%) - LeetHub

--- 325-maximum-size-subarray-sum-equals-k/325-maximum-size-subarray-sum-equals-k.java
+++ 325-maximum-size-subarray-sum-equals-k/325-maximum-size-subarray-sum-equals-k.java
@@ -5,8 +5,8 @@ class Solution {
         int ans=0;
         for(int i=0; i<nums.length; i++){
             sum +=nums[i];
-            if(sum==k)ans= i+1;
-            if(map.containsKey(sum-k)) ans =Math.max(i-map.get(sum-k),ans);
+            if(sum==k)ans =i+1;
+            if(map.containsKey(sum-k) && i-map.get(sum-k)>ans)ans =i-map.get(sum-k);
             if(!map.containsKey(sum)) map.put(sum,i);
         }
         
