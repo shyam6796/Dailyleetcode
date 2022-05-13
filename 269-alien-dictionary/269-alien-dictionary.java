a1ae5a58b7f5065d19531ac041a9f623e04f8a08
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2022-05-12 21:10:47 -0700

    Time: 3 ms (96.48%), Space: 41.9 MB (78.05%) - LeetHub

--- 269-alien-dictionary/269-alien-dictionary.java
+++ 269-alien-dictionary/269-alien-dictionary.java
@@ -1,67 +1,60 @@
 class Solution {
     public String alienOrder(String[] words) {
-        Map<Character,Set<Character>> map = new HashMap<>();
-        int indegree[] =new int[26];
-        buildGraph(map, indegree, words);
-        return bfs(map,indegree);
+        HashMap<Character,Set<Character>> map =new HashMap<>();
+        int arr[] =new int[26];
+        buildMap(map,arr,words);
+        return BFS(map,arr);
     }
     
-    public String bfs(Map<Character,Set<Character>> map, int indegree[]){
-        StringBuilder sb = new StringBuilder();
-        int totalchars =map.size();
+    public String BFS(HashMap<Character,Set<Character>> map,int arr[]){
+        StringBuilder sb =new StringBuilder();
+         int totalchars =map.size();
         Queue<Character> queue = new LinkedList<>();
         
-        for(char c : map.keySet()){
-            if(indegree[c-'a']==0){
-                queue.add(c);
-                
-            }
+        for(char c: map.keySet()){
+            if(arr[c-'a']==0)queue.add(c);
         }
         
         while(!queue.isEmpty()){
-            char c =queue.poll();
+            char c=queue.poll();
             sb.append(c);
-            for(char neg : map.get(c)){
-                indegree[neg -'a']--;
-                if(indegree[neg -'a'] ==0){
-
+            for( char neg : map.get(c)){
+                arr[neg -'a']--;
+                if(arr[neg-'a']==0){
                     queue.add(neg);
-                    
                 }
             }
         }
-        
-        return sb.length() == totalchars ? sb.toString() : "";
-        
+        return sb.length()==totalchars ? sb.toString() :"";
     }
     
-    public void buildGraph(Map<Character,Set<Character>> map, int indegree[], String[] words){
-         for(String word : words){
-             for(char c :word.toCharArray()){
+    public void  buildMap(HashMap<Character,Set<Character>> map,int arr[], String[] words){
+        for(String word :words){
+            for(char c:word.toCharArray()){
                  map.putIfAbsent(c,new HashSet<>());
-             }
-         }
+            }
+        }
         
-        for(int i=1; i<words.length; i++){
-            String first =words[i-1];
-            String second =words[i];
+        for(int i=0; i<words.length-1; i++){
+            String first = words[i];
+            String second = words[i+1];
             int len = Math.min(first.length(), second.length());
             for(int j=0; j<len; j++){
                 if(first.charAt(j) != second.charAt(j)){
                     char out =first.charAt(j);
-                    char in = second.charAt(j);
-                    if(!map.get(out).contains(in)){
+                    char in =second.charAt(j);
+                  if(!map.get(out).contains(in)){
                         map.get(out).add(in);
-                        indegree[in -'a']++;
+                        arr[in -'a']++;
                     }
                     break;
                 }
-                if (j + 1 == len && first.length() > second.length()) {
+                if(j+1 ==len && first.length() > second.length()){
                     map.clear();
                     return;
                 }
-                
             }
+            
         }
     }
 }
