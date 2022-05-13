b059784e5b0465faf9e3bc19afa06d638235aff1
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2022-05-12 18:44:18 -0700

    Time: 0 ms (100.00%), Space: 58.4 MB (38.33%) - LeetHub

--- 1060-missing-element-in-sorted-array/1060-missing-element-in-sorted-array.java
+++ 1060-missing-element-in-sorted-array/1060-missing-element-in-sorted-array.java
@@ -1,18 +1,18 @@
 class Solution {
     public int missingElement(int[] nums, int k) {
-        int left=0, right = nums.length-1, ref = nums[0]; // store the first element to make 
-		//first element-based answer like we have 0-based or 1-based array
-        while(left <= right) {
-            int mid = left + (right - left)/2;
-			//get the array element and subtract the reference and mid index - which 
-			//shows how many element are mising before array element
-            if(nums[mid] - (mid + ref) < k) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
+        int left =0;
+        int right =nums.length-1;
+        int ref=nums[0];
+        
+        while(left<=right){
+            int mid =left +(right-left)/2;
+            if(nums[mid] -(ref+mid) >=k){
+                right =mid-1;
+            }else{
+                left =mid+1;
             }
         }
-        return right + k + ref;
+        
+        return ref+right+k;
     }
-
 }
