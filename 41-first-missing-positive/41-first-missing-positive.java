90e37a948dd082e09b88454596d92900a135bd5b
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2022-05-19 00:54:17 -0700

    Time: 5 ms (46.57%), Space: 126.6 MB (63.80%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.java
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -1,18 +1,28 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-         long max=1;
-        HashSet<Integer> map=new HashSet<>();
-        for(int i=0;i<nums.length;i++){
-            map.add(nums[i]);
-            max=Math.max(max,nums[i]);
-        }
-        int res=0;
-        for(int i=1; i<=max+1;i++){
-            if(!map.contains(i)){
-                res=i;
-                break;
+        int n =nums.length;
+        
+        // swap the numbers to the correct indexes eg 2 should be present at index 1 as indexes start from 0;
+        for(int i=0;i<n;i++){
+            if(nums[i]>0 && nums[i]<=n){
+                int pos = nums[i]-1;
+                if(nums[pos]!=nums[i]){
+                    swap(nums,pos,i);
+                    i--;
+                }
             }
         }
-        return res;
+        
+        // find the missing value
+        for(int i=0;i<n;i++){
+            if(nums[i]!=i+1) return (i+1);
+        }
+        return n+1; // if all the nums are positive and present then the answer is n+1;
+    }
+    
+    public static void swap(int[] nums,int pos, int i){
+        int temp = nums[pos];
+        nums[pos] = nums[i];
+        nums[i] = temp;
     }
 }
