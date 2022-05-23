68135cf59a85fcce5f1201485b868adb65205c8c
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2022-05-22 23:53:58 -0700

    Time: 33 ms (46.89%), Space: 89.5 MB (6.14%) - LeetHub

--- 1242-web-crawler-multithreaded/1242-web-crawler-multithreaded.java
+++ 1242-web-crawler-multithreaded/1242-web-crawler-multithreaded.java
@@ -6,29 +6,62 @@
  * }
  */
 class Solution {
-    Set<String> result  = new HashSet<String>();
+    class Crawler implements Runnable{
+        public String startUrl;
+        public String hostName;
+        public HtmlParser htmlParser;
+        public static Set<String> set;
+        public static ExecutorService pool;
         
-    public List<String> crawl(String startUrl, HtmlParser htmlParser) {            
-        crawl(startUrl, htmlParser, startUrl);
-        List<String> finalResult = new ArrayList<String>();
-        finalResult.addAll(result);
-        return finalResult; 
-    }
-    
-    private void crawl(String startUrl, HtmlParser htmlParser, String currentUrl) {
-        result.add(currentUrl);
-        List<String> childUrls = htmlParser.getUrls(currentUrl);
+        public Crawler( String startUrl, String hostName, HtmlParser htmlParser){
+            this.startUrl =startUrl;
+            this.hostName =hostName;
+            this.htmlParser =htmlParser;
+        }
+        
+        @Override
+        public void run(){
+            if(startUrl.startsWith(hostName) && !set.contains(startUrl)){
+                set.add(startUrl);
+                List<Future<?>> list =new ArrayList<>();
+                for(String url :htmlParser.getUrls(startUrl)){
+                    Crawler crawler = new Crawler(url, hostName,htmlParser);
+                    list.add(pool.submit(crawler));
+                }
+                
+                for(Future<?> T : list){
+                    joinThread(T);
+                }
+            }
+        }
+        
+        public static void joinThread(Future<?> task){
+            try{
+               task.get();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+        
+    }  
+    public List<String> crawl(String startUrl, HtmlParser htmlParser) {
+        int index =startUrl.indexOf('/',7);
+        String hostName = index == -1 ?startUrl : startUrl.substring(0,index);
+        
+        Crawler crawler = new Crawler(startUrl,hostName, htmlParser);
+        ConcurrentHashMap<String,Boolean> map = new ConcurrentHashMap<>();
+        crawler.set =map.newKeySet();
             
-          Set<String> validChildren = new HashSet<String>();
-            
-           
-        childUrls.parallelStream().filter( url ->          
-              !result.contains(url) && getHostName(url).equals(getHostName(startUrl)) 
-            ).forEach(url -> crawl(startUrl, htmlParser, url));
-       
-    }
-
-    private String getHostName(String url){
-        return url.split("http://")[1].split("/")[0];
+        ExecutorService pool =  Executors.newFixedThreadPool(500);
+        crawler.pool =pool;
+        
+        Future<?> task = pool.submit(crawler);
+        crawler.joinThread(task);
+        pool.shutdown();
+        
+        
+        return new ArrayList<>(crawler.set);
+        
+        
     }
 }
