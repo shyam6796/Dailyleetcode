fe6168bebb8643651892bc5f4fd6946a1fa89090
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2022-06-02 23:24:49 -0700

    Time: 82 ms (22.67%), Space: 73.5 MB (45.62%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
@@ -1,23 +1,20 @@
 class Solution {
     public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
-        int n =startTime.length;
-        int dp[][] =new int[n][3];
-        TreeMap<Integer,Integer> map = new TreeMap<>();
+            int arr [][] = new int [startTime.length][3];
+        TreeMap<Integer,Integer> map =new TreeMap<>();
         map.put(0,0);
-        for(int i=0; i<n; i++){
-            dp[i][0] =startTime[i];
-            dp[i][1] =endTime[i];
-            dp[i][2] =profit[i];
-            
+        for(int i=0; i<startTime.length; i++){
+            arr[i][0]= startTime[i];
+            arr[i][1]= endTime[i];
+            arr[i][2]= profit[i];
         }
-         Arrays.sort(dp, (a,b) -> a[1]-b[1]);
-        for(int curr[] :dp){
-            int p =map.get(map.floorKey(curr[0])) +curr[2];
-            if(map.lastEntry().getValue() <=p){
-                map.put(curr[1],p);
+        Arrays.sort(arr,(a,b) -> a[1]- b[1]);
+        for(int profits[] : arr){
+            int total =map.get(map.floorKey(profits[0])) +profits[2];
+            if(map.get(map.lastKey())<total){
+                map.put(profits[1],total);
             }
         }
-        
-        return map.lastEntry().getValue();
+        return map.get(map.lastKey());
     }
 }
