91fd8457df48fc12998576b72bf20081e58c580c
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2022-06-03 20:00:32 -0700

    Time: 1503 ms (29.21%), Space: 68.4 MB (79.89%) - LeetHub

--- 1761-minimum-degree-of-a-connected-trio-in-a-graph/1761-minimum-degree-of-a-connected-trio-in-a-graph.java
+++ 1761-minimum-degree-of-a-connected-trio-in-a-graph/1761-minimum-degree-of-a-connected-trio-in-a-graph.java
@@ -1,27 +1,29 @@
 class Solution {
     public int minTrioDegree(int n, int[][] edges) {
-         HashSet<Integer>[] adjList = new HashSet[n];
-        
-        for(int i = 0; i < n; i++)
-            adjList[i] = new HashSet<>();
-        
-        for(int[] edge: edges) {
-            adjList[edge[0] - 1].add(edge[1] - 1);
-            adjList[edge[1] - 1].add(edge[0] - 1);
+         //adjecency matrix
+        Map<Integer, HashSet<Integer>> graph = new HashMap<>();
+        for(int[] e : edges) {
+            //2 lines approach
+            graph.compute(e[0], (k,v) -> v == null ? new HashSet<Integer>() : v).add(e[1]);
+            graph.compute(e[1], (k,v) -> v == null ? new HashSet<Integer>() : v).add(e[0]);
         }
-        
-        int min = Integer.MAX_VALUE;
-        
-        for(int i = 0; i < n; i++) {
-            for(int j = i + 1; j < n; j++) {
-                if(!adjList[i].contains(j)) // optimisation 
-                    continue;
-                for(int k = j + 1; k < n; k++)
-                     if(adjList[j].contains(k) && adjList[i].contains(k))
-                         min = Math.min(min, adjList[i].size() + adjList[j].size() + adjList[k].size() - 6);
+        int degree = Integer.MAX_VALUE;
+        for(int[] e : edges) {
+            HashSet<Integer> x = graph.get(e[0]);
+            HashSet<Integer> y = graph.get(e[1]);
+            
+            for(int num : x) {
+                if(y.contains(num)) { // found trio
+                    // degree = (total edges from x) 
+                    //        + (total edges from y) + (total edges from num) - 6
+                    // why 6? : If Trio degree is ZERO then it will have 6 edges 
+                    // i.e. count for edge from each connected node in Trio
+                    int total = x.size() + y.size() + graph.get(num).size() - 6;
+                    degree = Math.min(degree, total);
+                }
             }
         }
         
-        return min == Integer.MAX_VALUE? -1: min;
+        return degree == Integer.MAX_VALUE ? -1 : degree;
     }
 }
