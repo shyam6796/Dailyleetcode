657a446bec5bb5053f4130d50027e518e55fb6cc
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2022-06-04 15:41:54 -0700

    Time: 144 ms (56.42%), Space: 122 MB (5.26%) - LeetHub

---
+++ 1166-design-file-system/1166-design-file-system.java
@@ -0,0 +1,36 @@
+class FileSystem {
+    Map<String,Integer> map;
+    public FileSystem() {
+        map =new HashMap<>();
+    }
+    
+    public boolean createPath(String path, int value) {
+        if(path.equals("") ||path.equals("/") ||map.containsKey(path)) return false;
+        
+        String dict[] =path.split("/");
+        StringBuilder sb =new StringBuilder("/");
+        
+        for(int i=1; i<dict.length-1; i++){
+            String token =dict[i];
+            sb.append(token);
+            String parent = sb.toString();
+            if(!map.containsKey(parent)) return false; 
+            sb.append("/");
+        }
+        map.put(path,value);
+        return true;
+    }
+    
+    public int get(String path) {
+     if(map.containsKey(path))return map.get(path);
+    
+        return -1;
+    }
+}
+
+/**
+ * Your FileSystem object will be instantiated and called as such:
+ * FileSystem obj = new FileSystem();
+ * boolean param_1 = obj.createPath(path,value);
+ * int param_2 = obj.get(path);
+ */
