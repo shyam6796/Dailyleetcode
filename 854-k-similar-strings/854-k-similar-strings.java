e148a89cfa010aea547106869bfe2070d3be071a
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2022-06-04 22:02:42 -0700

    Time: 304 ms (8.93%), Space: 119.2 MB (7.70%) - LeetHub

---
+++ 854-k-similar-strings/854-k-similar-strings.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int kSimilarity(String s1, String tar) {
+        Queue<String> q = new ArrayDeque<>();
+        q.add(s1);
+        
+        int lvl = 0;
+        while(q.size() > 0){
+            int size = q.size();
+            while(size-- > 0){
+                String s = q.remove();
+                if(s.equals(tar))return lvl;
+                
+                
+                int i = 0;
+                while(s.charAt(i) == tar.charAt(i))i++;
+                
+                int j = i;
+                
+                while(j < s.length()){
+                    if(s.charAt(j) == tar.charAt(i) && tar.charAt(j) != s.charAt(j)){
+                        StringBuilder sb = new StringBuilder(s);
+                        sb.setCharAt(i, s.charAt(j));
+                        sb.setCharAt(j, s.charAt(i));
+                        
+						//A small optimization.
+                        if(sb.toString().equals(tar))return lvl+1;
+                        
+                        q.add(sb.toString());
+                    }
+                    j++;
+                }
+            }
+            lvl++;
+        }
+        return lvl;
+    }
+}
