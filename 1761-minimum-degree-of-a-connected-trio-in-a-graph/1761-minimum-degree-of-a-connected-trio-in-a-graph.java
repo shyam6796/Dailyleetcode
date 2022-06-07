8d6bcf6ee631f03bb19ea44b6816ef8a2b16a47f
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2022-06-06 18:07:14 -0700

    Time: 996 ms (45.96%), Space: 266.4 MB (21.16%) - LeetHub

--- 1761-minimum-degree-of-a-connected-trio-in-a-graph/1761-minimum-degree-of-a-connected-trio-in-a-graph.java
+++ 1761-minimum-degree-of-a-connected-trio-in-a-graph/1761-minimum-degree-of-a-connected-trio-in-a-graph.java
@@ -1,29 +1,27 @@
 class Solution {
     public int minTrioDegree(int n, int[][] edges) {
-         //adjecency matrix
-        Map<Integer, HashSet<Integer>> graph = new HashMap<>();
-        for(int[] e : edges) {
-            //2 lines approach
-            graph.compute(e[0], (k,v) -> v == null ? new HashSet<Integer>() : v).add(e[1]);
-            graph.compute(e[1], (k,v) -> v == null ? new HashSet<Integer>() : v).add(e[0]);
+         HashSet<Integer>[] adjList = new HashSet[n+1];
+        
+        for(int i = 1; i <=n; i++)
+            adjList[i] = new HashSet<>();
+        
+        for(int[] edge: edges) {
+            adjList[edge[0] ].add(edge[1] );
+            adjList[edge[1] ].add(edge[0]);
         }
-        int degree = Integer.MAX_VALUE;
-        for(int[] e : edges) {
-            HashSet<Integer> x = graph.get(e[0]);
-            HashSet<Integer> y = graph.get(e[1]);
-            
-            for(int num : x) {
-                if(y.contains(num)) { // found trio
-                    // degree = (total edges from x) 
-                    //        + (total edges from y) + (total edges from num) - 6
-                    // why 6? : If Trio degree is ZERO then it will have 6 edges 
-                    // i.e. count for edge from each connected node in Trio
-                    int total = x.size() + y.size() + graph.get(num).size() - 6;
-                    degree = Math.min(degree, total);
-                }
+        
+        int min = Integer.MAX_VALUE;
+        
+        for(int i = 1; i <= n; i++) {
+            for(int j = i + 1; j <= n; j++) {
+                if(!adjList[i].contains(j)) // optimisation 
+                    continue;
+                for(int k = j + 1; k <= n; k++)
+                     if(adjList[j].contains(k) && adjList[i].contains(k))
+                         min = Math.min(min, adjList[i].size() + adjList[j].size() + adjList[k].size() - 6);
             }
         }
         
-        return degree == Integer.MAX_VALUE ? -1 : degree;
+        return min == Integer.MAX_VALUE? -1: min;
     }
 }
