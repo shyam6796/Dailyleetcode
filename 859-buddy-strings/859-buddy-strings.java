2b91db4d1ddd4edb45fb37770954d5c38a8ec403
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2022-06-12 16:36:47 -0700

    Time: 4 ms (46.67%), Space: 42.5 MB (66.94%) - LeetHub

--- 859-buddy-strings/859-buddy-strings.java
+++ 859-buddy-strings/859-buddy-strings.java
@@ -1,29 +1,28 @@
 class Solution {
     public boolean buddyStrings(String s, String goal) {
-       if (s.length() != goal.length()) return false;
-        if (s.equals(goal)) {
-            int[] count = new int[26];
-            for (int i = 0; i < s.length(); ++i)
-                count[s.charAt(i) - 'a']++;
-
-            for (int c: count)
-                if (c > 1) return true;
-            return false;
-        } else {
-            int first = -1, second = -1;
-            for (int i = 0; i < s.length(); ++i) {
-                if (s.charAt(i) != goal.charAt(i)) {
-                    if (first == -1)
-                        first = i;
-                    else if (second == -1)
-                        second = i;
-                    else
-                        return false;
-                }
+        if (s.length() != goal.length()) return false;
+        if(s.equals(goal)){
+            int [] arr =new int[26];
+            for(int i=0; i<s.length(); i++){
+                arr[s.charAt(i)-'a']++;
             }
-
-            return (second != -1 && s.charAt(first) == goal.charAt(second) &&
-                    s.charAt(second) == goal.charAt(first));
+            for(int i :arr){
+                if(i>1)return true;
+            }
+            return false;
         }
+        int first =-1, second =-1;
+        
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)==goal.charAt(i))continue;
+            else{
+                if(first==-1){
+                    first=i;
+                }else if(second ==-1){
+                    second = i;
+                }else return false;
+            }
+        }
+        return (second != -1 && s.charAt(first)==goal.charAt(second) && s.charAt(second)==goal.charAt(first));
     }
 }
