6f7b0100a73b0e43b3bf4c9f8593efc51f21cd10
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2022-06-12 17:49:52 -0700

    Time: 152 ms (91.19%), Space: 102.5 MB (65.10%) - LeetHub

--- 317-shortest-distance-from-all-buildings/317-shortest-distance-from-all-buildings.java
+++ 317-shortest-distance-from-all-buildings/317-shortest-distance-from-all-buildings.java
@@ -1,61 +1,63 @@
 class Solution {
- 
-    int dirs[][]={{0,1},{1,0},{-1,0},{0,-1}};
+    int dirs [][] = {{1,0},{0,1},{-1,0},{0,-1}};
+     int m,n;
     public int shortestDistance(int[][] grid) {
-        int m=grid.length;
-        int n=grid[0].length;
-        int dist[][] =new int[m][n];
-         int reach[][] =new int[m][n];
-        int totalBuilding=0;
-        
-        for(int i=0; i<m; i++){
+        m =grid.length;
+        n =grid[0].length;
+        int reach [][] =new int[m][n];
+        int dist [][] =new int[m][n];
+        int totalBuildings=0;
+        for(int i =0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(grid[i][j]==1){
-                    if(!helper(grid,dist,reach,i,j))return -1;
-                    totalBuilding++;
+                    if(!BFS(grid,dist,reach,i,j))return -1;
+                    totalBuildings++;
                 }
             }
         }
         int ans=Integer.MAX_VALUE;
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-               if(dist[i][j] <ans && reach[i][j]==totalBuilding){
-                        ans= dist[i][j];
-                    } 
+                if(dist[i][j] <ans && reach[i][j] ==totalBuildings){
+                    ans =dist[i][j];
+                }
             }
         }
-        return ans==Integer.MAX_VALUE ? -1 :ans;
+        return ans ==Integer.MAX_VALUE ? -1 :ans;
     }
-    public boolean helper(int [][] grid, int [][] dist, int [][] reach, int i, int j){
-        int m=grid.length;
-        int n=grid[0].length;
+    
+    public boolean BFS(int [][] grid, int [][]dist, int[][]reach, int x, int y){
+        Queue<int[]> queue =new LinkedList<>();
         boolean visited[][] =new boolean[m][n];
-        Queue<int[]>queue =new LinkedList<>();
-        queue.add(new int[]{i,j});
-        visited[i][j]=true;
+        visited[x][y] =true;
+        queue.add(new int []{x,y});
         int d=0;
         while(!queue.isEmpty()){
             d++;
             int size=queue.size();
-            for(int k=0; k<size; k++){
-                int []curr =queue.poll();
-                for(int [] dir :dirs){
-                    int x= curr[0]+dir[0];
-                    int y= curr[1]+dir[1];
-                    if(x < 0 || x>=m || y <0 || y >=n ||grid[i][j] ==2 || visited[x][y] )continue;
-                     if(grid[x][y]==0){
-                        reach[x][y]++;
-                       dist[x][y] += d;
-                        queue.add(new int[]{x,y});
+            for(int i=0; i<size; i++){
+                int [] curr= queue.poll();
+                
+                for(int dir [] :dirs){
+                    int newX =curr[0]+dir[0];
+                    int newY =curr[1]+dir[1];
+                    
+                    if(newX <0 || newX >=m || newY <0 || newY >=n ||grid[newX][newY]==2 || visited[newX][newY]) continue;
+                    if(grid[newX][newY]==0){
+                        dist[newX][newY] +=d;
+                        reach[newX][newY]++;
+                        
+                        queue.add(new int[]{newX,newY});
                     }
-                    visited[x][y]=true;
+                    visited[newX][newY]=true;
+                    
                 }
             }
         }
         
-        for(int row=0; row<m; row++){
-            for(int col=0; col<n; col++){
-                if(grid[row][col]==1 && !visited[row][col])return false;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(!visited[i][j] && grid[i][j] ==1) return false;
             }
         }
         return true;
