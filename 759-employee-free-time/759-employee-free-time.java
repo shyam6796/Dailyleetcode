b1ded9bcefc6075b926280a54e4b9d519dc1d3a9
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2022-06-13 20:31:25 -0700

    Time: 17 ms (49.95%), Space: 54.7 MB (51.42%) - LeetHub

--- 759-employee-free-time/759-employee-free-time.java
+++ 759-employee-free-time/759-employee-free-time.java
@@ -15,30 +15,26 @@ class Interval {
 
 class Solution {
     public List<Interval> employeeFreeTime(List<List<Interval>> schedule) {
-        List<Interval> list =new ArrayList<>();
-        List<Interval> ans = new ArrayList<>();
-        
+        List<Interval> list = new ArrayList<>();
+        List<Interval> ans =new ArrayList<>();
         for(List<Interval> li :schedule){
-            for(int i=0; i<li.size(); i++){
-                list.add(li.get(i));
-            }
+            list.addAll(li);
         }
+        Collections.sort(list,(a,b) -> a.start-b.start);
         
-          Collections.sort(list, (a,b)-> a.start-b.start);
-        
-        int preStart = list.get(0).start;
-        int preEnd = list.get(0).end;
+        int preStart =list.get(0).start;
+        int preEnd =list.get(0).end;
         
         for(int i=1; i<list.size(); i++){
             int currStart = list.get(i).start;        
             int currEnd = list.get(i).end;
-            if(currStart <= preEnd){
-                preEnd =Math.max(preEnd, currEnd);
+            if(list.get(i).start<=preEnd){
+               preEnd =Math.max(preEnd, currEnd);
+                //continue;
             }else{
                 ans.add(new Interval(preEnd,currStart));
                 preStart = currStart;
                 preEnd = currEnd;
-                    
             }
         }
         return ans;
