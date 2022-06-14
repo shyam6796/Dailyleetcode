46befad9d289948eb46fb720160b79b57d771d5f
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2022-06-13 20:51:35 -0700

    Time: 250 ms (7.68%), Space: 120.7 MB (24.04%) - LeetHub

--- 1166-design-file-system/1166-design-file-system.java
+++ 1166-design-file-system/1166-design-file-system.java
@@ -1,30 +1,27 @@
 class FileSystem {
-    Map<String,Integer> map;
+    HashMap<String,Integer> map ;
     public FileSystem() {
         map =new HashMap<>();
     }
     
     public boolean createPath(String path, int value) {
-        if(path.equals("") ||path.equals("/") ||map.containsKey(path)) return false;
+        if(path.equals("") || path.equals("/") || map.containsKey(path)) return false;
+        String tokens [] = path.split("/");
+        StringBuilder sb = new StringBuilder();
+        sb.append("/");
         
-        String dict[] =path.split("/");
-        StringBuilder sb =new StringBuilder("/");
-        
-        for(int i=1; i<dict.length-1; i++){
-            String token =dict[i];
-            sb.append(token);
-           
-            if(!map.containsKey(sb.toString())) return false; 
+        for(int i=1; i<tokens.length-1; i++){
+            sb.append(tokens[i]);
+            if(!map.containsKey(sb.toString())) return false;
             sb.append("/");
         }
+        
         map.put(path,value);
         return true;
     }
     
     public int get(String path) {
-     if(map.containsKey(path))return map.get(path);
-    
-        return -1;
+      return map.containsKey(path) ? map.get(path) : -1;
     }
 }
 
