7900bf045b52d6d0660d54e273814613ba94faad
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2022-06-14 23:07:44 -0700

    Time: 43 ms (45.38%), Space: 67.8 MB (43.24%) - LeetHub

--- 1229-meeting-scheduler/1229-meeting-scheduler.java
+++ 1229-meeting-scheduler/1229-meeting-scheduler.java
@@ -4,20 +4,19 @@ class Solution {
         int i=0, j=0;
         Arrays.sort(slots1, (a, b) -> a[0] - b[0]);
         Arrays.sort(slots2, (a, b) -> a[0] - b[0]);
-        
-        while(i<slots1.length && j <slots2.length){
+        while(i <slots1.length && j < slots2.length){
             int start =Math.max(slots1[i][0],slots2[j][0]);
-            int end = Math.min(slots1[i][1],slots2[j][1]);
-            //System.out.println(start +"  "+ end);
-            if(end -start >=duration){
+             int end =Math.min(slots1[i][1],slots2[j][1]);
+            if(end-start >=duration){
                 list.add(start);
                 list.add(start+duration);
                 break;
-            }
-            if(slots1[i][1] >slots2[j][1]){
-                j++;
             }else{
-                i++;
+                if(slots1[i][1] > slots2[j][1]){
+                    j++;
+                }else{
+                    i++;
+                }
             }
         }
         return list;
