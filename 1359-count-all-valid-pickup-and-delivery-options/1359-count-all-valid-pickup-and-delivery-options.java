7002ce2f156df73f9a28ff87d1f614c4150c0217
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2022-06-14 22:57:27 -0700

    Time: 1 ms (45.79%), Space: 42.1 MB (25.54%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.java
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.java
@@ -1,14 +1,15 @@
 class Solution {
     public int countOrders(int n) {
-        long dp [] = new long[501];
-        long mod =1000_000_007;
-        dp[1] = 1L;
-        dp[2] = 6L;
+        if(n<2)return 1;
+        long dp[] =new long[n+1];
+        dp[1]=1L;
+        dp[2]=6L;
+        long mod = 1000_000_007;
         for(int i=3; i<=n; i++){
-            int place = 2*i -1;
-            int perm = place*(place+1)/2;
-            dp[i] =(dp[i-1] *perm) % mod;
+            long place =2*i-1;
+            long perm = place*(place+1)/2;
+            dp[i]= (dp[i-1]*perm)%mod;
         }
-        return (int)dp[n];    
+        return (int)dp[n];
     }
 }
