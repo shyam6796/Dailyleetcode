bf7d68330f990eaaa0ca7b1591a6294589c3af07
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2022-06-14 22:50:23 -0700

    Time: 71 ms (28.95%), Space: 145.6 MB (30.29%) - LeetHub

---
+++ 239-sliding-window-maximum/239-sliding-window-maximum.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        LinkedList<Integer> list =new LinkedList<>();
+        int ans [] = new int[nums.length-k+1];
+        
+        int j=0;
+        for(int i=0; i<nums.length; i++){
+            while(!list.isEmpty() && list.getFirst() <= i-k){
+                list.removeFirst();
+            }
+            while(!list.isEmpty() &&  nums[i] >= nums[list.getLast()]){
+                list.removeLast();
+            }
+            list.addLast(i);
+            
+            if(i >=k-1){
+                ans[j] =nums[list.getFirst()];
+                j++;
+            }
+        }
+        return ans;
+    }
+}
