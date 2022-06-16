33e4af114694bd9e03961fc2a97d20ed680adb36
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2022-06-15 22:50:39 -0700

    Time: 36 ms (59.04%), Space: 49.1 MB (23.21%) - LeetHub

--- 1152-analyze-user-website-visit-pattern/1152-analyze-user-website-visit-pattern.java
+++ 1152-analyze-user-website-visit-pattern/1152-analyze-user-website-visit-pattern.java
@@ -1,62 +1,53 @@
 class Solution {
     public List<String> mostVisitedPattern(String[] username, int[] timestamp, String[] website) {
-        List<List<String>> list = new ArrayList<>();
-        
+        List<List<String>> list =new ArrayList<>();
         for(int i=0; i<username.length; i++){
             List<String> log =new ArrayList<>();
             log.add(username[i]);
-            log.add(timestamp[i]+"");
-            log.add(website[i]);
+             log.add(timestamp[i]+"");
+             log.add(website[i]);
             list.add(log);
         }
-        
-     Collections.sort(list, (a,b) ->  Integer.valueOf(a.get(1)) -Integer.valueOf(b.get(1)));
-        
+        Collections.sort(list,(a,b) -> Integer.valueOf(a.get(1)) - Integer.valueOf(b.get(1)));
         Map<String,List<String>> map =new HashMap<>();
-        
-        for(List<String>log : list){
-            List<String> pageList = map.getOrDefault(log.get(0), new ArrayList<>());
-            pageList.add(log.get(2));
-            map.put(log.get(0),pageList);
+        for(List<String> log : list){
+            List<String> pages =map.getOrDefault(log.get(0),new ArrayList<>());
+            pages.add(log.get(2));
+            map.put(log.get(0),pages);
         }
         
-        Map<String,Integer> counts =new HashMap<>();
-         int max=0;
-         String freqstring ="";
+        HashMap<String,Integer> count =new HashMap<>();
+        int max=0;
+        String freqString="";
         for(String user : map.keySet()){
-            Set<String> seq = helper(map.get(user));
-            if(seq ==null)continue;
-            
-            for(String s :seq){
-                counts.put(s, counts.getOrDefault(s,0)+1);
-                if(counts.get(s)>max){
-                    freqstring =s;
-                    max=counts.get(s);
-                }else if(counts.get(s)==max){
-                    freqstring = s.compareTo(freqstring) < 0 ? s : freqstring ;
-                }           
+            HashSet<String> set =getPages(map.get(user));
+            if(set ==null)continue;
+            for(String s :set){
+                count.put(s,count.getOrDefault(s,0)+1);
+                if(max <count.get(s)){
+                    freqString =s;
+                    max =count.get(s);
+                }else if(max ==count.get(s)){
+                    freqString = s.compareTo(freqString) >=0 ?freqString : s;
+                }
             }
+            
         }
-        
-        return Arrays.asList(freqstring.split(","));
-        
+        return Arrays.asList(freqString.split(","));
     }
     
-    public Set<String> helper(List<String> list){
-        if(list.size()<3)return null;
-        Set<String> set =new HashSet<>();
-        StringBuilder sb;
-        for(int i=0; i<list.size()-2; i++){
-            
-            for(int j=i+1; j<list.size()-1; j++){
-                 
-                for(int k=j+1; k<list.size(); k++){
-                    sb =new StringBuilder();
-                    sb.append(list.get(i));
+    public HashSet<String> getPages(List<String> pages){
+        if(pages.size() < 3)return null;
+        HashSet<String> set =new HashSet<>();
+        for(int i=0; i<pages.size()-2; i++){
+            for(int j=i+1; j<pages.size()-1; j++){
+                for(int k=j+1; k<pages.size(); k++){
+                    StringBuilder sb =new StringBuilder();
+                    sb.append(pages.get(i));
                     sb.append(",");
-                    sb.append(list.get(j));
+                    sb.append(pages.get(j));
                     sb.append(",");
-                    sb.append(list.get(k));
+                    sb.append(pages.get(k));
                     set.add(sb.toString());
                 }
             }
