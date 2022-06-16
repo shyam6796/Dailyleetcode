37f1a65747d2ee73a6e28625b3976b38ac7d6fd1
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2022-06-15 22:37:18 -0700

    Time: 2 ms (75.75%), Space: 49.5 MB (45.30%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -1,16 +1,13 @@
 class Solution {
     public int jump(int[] nums) {
-         int jumps = 0, currentJumpEnd = 0, farthest = 0;
-        for (int i = 0; i < nums.length - 1; i++) {
-            // we continuously find the how far we can reach in the current jump
-            farthest = Math.max(farthest, i + nums[i]);
-            // if we have come to the end of the current jump,
-            // we need to make another jump
-            if (i == currentJumpEnd) {
-                jumps++;
-                currentJumpEnd = farthest;
+        int count=0, fatrhest=0, currentJumpEnd=0;
+        for(int i=0; i<nums.length-1; i++){
+            fatrhest =Math.max(fatrhest,i+nums[i]);
+            if(i==currentJumpEnd){
+                currentJumpEnd=fatrhest;
+                count++;
             }
         }
-        return jumps;
+        return count;
     }
 }
