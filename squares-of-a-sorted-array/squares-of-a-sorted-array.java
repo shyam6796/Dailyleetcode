371508bc464177faed14ac833c7dc8a36a86734d
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2022-07-14 19:29:11 -0700

    Time: 2 ms, Memory: 55.9 MB - LeetHub

--- squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,9 +1,21 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        for(int i=0; i<nums.length; i++){
-            nums[i]=nums[i]*nums[i];
+        int n = nums.length;
+        int[] result = new int[n];
+        int left = 0;
+        int right = n - 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            int square;
+            if (Math.abs(nums[left]) < Math.abs(nums[right])) {
+                square = nums[right];
+                right--;
+            } else {
+                square = nums[left];
+                left++;
+            }
+            result[i] = square * square;
         }
-        Arrays.sort(nums);
-        return nums;
+        return result;
     }
 }
