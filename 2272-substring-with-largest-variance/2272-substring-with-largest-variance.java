c759c7edea1bc2019033ee587f4c2722fdd6cca5
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2022-10-04 19:14:06 -0700

    Time: 264 ms (70.30%), Space: 90.9 MB (25.78%) - LeetHub

--- 2272-substring-with-largest-variance/2272-substring-with-largest-variance.java
+++ 2272-substring-with-largest-variance/2272-substring-with-largest-variance.java
@@ -1,43 +1,39 @@
 class Solution {
     public int largestVariance(String s) {
- // Maintain a map of freq of characters in the string
-        Map<Character, Integer> map = new HashMap<>();
-        for(char c : s.toCharArray())
-            map.put(c, map.getOrDefault(c, 0) + 1);
+       HashMap<Character,Integer> map =new HashMap<>();
         
-        int max=0;
-		// Check for every possible pair of characters in the map with the assumption that the one char is greater than the other
-		// In the following piece of code, assuming c2 count is greater than c1
-        for(char c1 : map.keySet()){
-            for(char c2 : map.keySet()){
-			    // If both the characters are same then we don't explore any further
-                if(c1 == c2)
+        for(char c : s.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+        
+        int variance=0;
+        
+        for(char c1 :map.keySet()){
+            for(char c2 :map.keySet()){
+                if(c1==c2){
                     continue;
-                int c1Freq=0, c2Freq=0;
-                int c1Remaining=map.get(c1); // Keep track of the remaining c1 chars
-				
-				// Iterate through all the characters in the string
-                for(char c : s.toCharArray()){
-                    if(c == c1){
-                        c1Freq++;
-                        c1Remaining--;
+                }
+                int c1freq=0, c2freq=0;
+                int c1rem =map.get(c1);
+                for(char c :s.toCharArray()){
+                    if(c==c1){
+                        c1freq++;
+                        c1rem--;
                     }
-                    if(c == c2)
-                        c2Freq++;
-
-					// If c2-count < c1-count then we reset the counters, only if we know there are more c1 chars to come in the iteration
-					// c1Remaining check is required for the test case "baa" and c1=b && c2=a. We don't reset the counters if there are no more c1 chars left					
-                    if(c2Freq < c1Freq && c1Remaining > 0) {
-                        c2Freq=0;
-                        c1Freq=0;
+                    if(c==c2){
+                        c2freq++;
                     }
                     
-					// Calculate variance of current substring and update max accordingly
-                    if(c1Freq > 0 && c2Freq > 0)
-                        max = Math.max(max, c2Freq-c1Freq);
+                    if(c1freq >c2freq && c1rem >0){
+                        c1freq=0;
+                        c2freq=0;
+                    }
+                    if(c1freq >0 && c2freq >0){
+                        variance =Math.max(variance,c2freq-c1freq);
+                    }
                 }
             }
         }
-        return max;
+        return variance;
     }
 }
