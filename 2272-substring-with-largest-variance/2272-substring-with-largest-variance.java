ad27195bc69f72934c17b695b08e42c13e900c36
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2022-10-27 21:44:00 -0700

    Time: 343 ms (40.48%), Space: 94.9 MB (14.51%) - LeetHub

--- 2272-substring-with-largest-variance/2272-substring-with-largest-variance.java
+++ 2272-substring-with-largest-variance/2272-substring-with-largest-variance.java
@@ -2,38 +2,41 @@ class Solution {
     public int largestVariance(String s) {
        HashMap<Character,Integer> map =new HashMap<>();
         
-        for(char c : s.toCharArray()){
+    
+        for (char c : s.toCharArray()){
             map.put(c,map.getOrDefault(c,0)+1);
         }
         
-        int variance=0;
+        int varaince =0;
         
         for(char c1 :map.keySet()){
-            for(char c2 :map.keySet()){
-                if(c1==c2){
+            for(char c2 : map.keySet()){
+                if(c1 ==c2){
                     continue;
                 }
+                
                 int c1freq=0, c2freq=0;
-                int c1rem =map.get(c1);
+                int c1ream =map.get(c1);
+                
                 for(char c :s.toCharArray()){
                     if(c==c1){
                         c1freq++;
-                        c1rem--;
+                        c1ream--;
                     }
                     if(c==c2){
                         c2freq++;
                     }
                     
-                    if(c1freq >c2freq && c1rem >0){
+                    if(c2freq <c1freq && c1ream >0){
                         c1freq=0;
                         c2freq=0;
                     }
-                    if(c1freq >0 && c2freq >0){
-                        variance =Math.max(variance,c2freq-c1freq);
-                    }
+                    if(c1freq > 0 && c2freq > 0)
+                    varaince = Math.max(varaince,c2freq -c1freq);
                 }
             }
         }
-        return variance;
-    }
+        
+        return varaince;
+      }
 }
