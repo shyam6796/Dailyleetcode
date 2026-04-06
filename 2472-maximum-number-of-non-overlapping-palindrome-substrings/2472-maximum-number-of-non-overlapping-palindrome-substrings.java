4d3ef7bab0f2aeb7e8ab47bd89da916a95bce70b
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2026-04-06 11:57:49 -0500

    Time: 2441 ms (5.15%), Space: 205.2 MB (5.64%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
@@ -1,32 +1,29 @@
 class Solution {
-    Integer memo [][];
+    Integer memo[][];
     public int maxPalindromes(String s, int k) {
-         if(k==1)return s.length();
-        int n = s.length();
-        memo = new Integer[n][n];
-
-        return helper(s,n,0,k-1,k);
+        if(k==1)return s.length();
+        memo = new Integer[s.length()][s.length()];
+       return  helper(s,0,k-1,k);
     }
-    public int helper(String s, int n, int i, int j, int k){
-        if(i >=s.length() || j >=s.length()){
-            return 0;
-        }
+
+    public int helper(String s, int i, int j, int k){
+        if(j >=s.length() || i >=s.length())return 0;
 
         if(memo[i][j] !=null)return memo[i][j];
 
-        if(isValid(s.substring(i,j+1))){
-           return memo[i][j] =  Math.max(1+ helper(s,n,j+1,j+k,k), Math.max(helper(s,n,i,j+1,k), helper(s,n,i+1,j+1,k)));
+        //int ans =0;
+        if(isPalindrome(s, i, j)){// 1 + always next windo
+          return memo[i][j] =Math.max(1+ helper(s, j+1, j+k, k), Math.max(helper(s,i,j+1,k), helper(s, i+1, j+1, k)));
         }
-        return memo[i][j] =  Math.max(helper(s,n,i,j+1,k), helper(s, n, i+1, j+1, k));
-    }
+        return memo[i][j] = Math.max(helper(s,i,j+1,k),  helper(s, i+1, j+1, k));
 
-    public boolean isValid(String s){
-        int left =0;
-        int right =s.length()-1;
-        while(left <right){
-            if(s.charAt(left ) != s.charAt(right))return false;
-            left++;
-            right--;
+
+    }
+    private boolean isPalindrome(String s, int start, int end) {
+          while (start < end) {
+            if (s.charAt(start++) != s.charAt(end--)) {
+                return false;
+            }
         }
         return true;
     }
