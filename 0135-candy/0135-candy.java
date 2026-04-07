88d4f5b57aad39aea7c3c68a07e1f171aab555a3
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2026-04-07 14:10:53 -0500

    Time: 3 ms (86.83%), Space: 47.8 MB (40.3%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,24 +1,29 @@
 class Solution {
     public int candy(int[] ratings) {
-        int left[] =new int[ratings.length];
-        int right[]  =new int[ratings.length];
-        int ans =0;
-         left[0]=1;
+        int ans =1;
+        int i=1;
+        
+        while(i < ratings.length){
+            if(ratings[i]==ratings[i-1]){
+                ans++;
+                i++;
+                continue;
+            }
+            int up=1;
+            while(i < ratings.length && ratings[i] > ratings[i-1]){
+                up++;
+                ans +=up;
+                i++;
+            }
 
-        for(int i =1; i <ratings.length; i++){
-            if(ratings[i] > ratings[i-1]){
-                left[i]  = 1+ left[i-1];
-            }else left[i]=1;
-        }
-        right[ratings.length-1] =1;
-        for(int i =ratings.length-2; i >=0; i--){
-            if(ratings[i] > ratings[i+1]){
-                right[i] =1+right[i+1];
-            }else right[i] =1;
-        }
+            int down =1;
+            while(i < ratings.length && ratings[i-1] > ratings[i]){
+                ans +=down;
+                down++;
+                i++;
+            }
+            if(down - up >0)ans +=down -up;
 
-        for(int i =0; i< ratings.length; i++){
-            ans +=Math.max(left[i],right[i]);
         }
         return ans;
     }
