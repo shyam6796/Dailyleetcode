ea6d564d73ffe131bbe8aa0200f53c64ff46a4e8
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2026-04-18 12:31:32 -0500

    Time: 1 ms (100%), Space: 44.1 MB (74.56%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,32 +1,30 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int max =0;
+        int ans =0;
 
         for(int i =0; i < grid.length; i++){
             for(int j =0; j < grid[0].length; j++){
-                    if(grid[i][j] ==2){
-                        helper(grid,i,j,2);
-                    }
+                 if(grid[i][j] ==2) DFS(grid, i,j,2);
             }
         }
 
-        for(int i =0; i < grid.length; i++){
-            for(int j=0; j <grid[0].length; j++){
+        for(int i =0; i < grid.length; i++ ){
+            for(int j =0; j < grid[0].length; j++){
                 if(grid[i][j]==1)return -1;
-                max = Math.max(grid[i][j],max);
+                ans =Math.max(ans,grid[i][j]);
             }
         }
-        return max == 0 ? 0 : max-2;
+
+        return ans ==0 ? 0 : ans-2;
     }
 
-    public void helper(int [][] grid, int i, int j ,int id){
-        if(i < 0 || j < 0 || i >=grid.length || j >= grid[0].length || (grid[i][j] < id && grid[i][j] !=1))return ;
-        grid[i][j] = id;
+    public void DFS(int [][] grid, int i, int j, int time){
+        if(i < 0 || j < 0 || i >=grid.length  || j >=grid[0].length || (grid[i][j] < time && grid[i][j] !=1))return ;
+        grid[i][j] =time;
 
-        helper(grid,i+1,j,id+1);
-        helper(grid,i,j+1,id+1);
-        helper(grid,i-1,j,id+1);
-        helper(grid,i,j-1,id+1);
-
+        DFS(grid, i+1, j, time+1);  
+        DFS(grid, i, j-1, time+1);
+        DFS(grid, i, j+1, time+1);
+        DFS(grid, i-1, j, time+1);
     }
 }
