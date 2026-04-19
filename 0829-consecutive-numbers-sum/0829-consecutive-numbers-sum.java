d3b74dc1c381ea91245109fd98b9e9774d680493
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2026-04-19 14:37:42 -0500

    Time: 4 ms (74.31%), Space: 42.1 MB (61.11%) - LeetHub

--- 0829-consecutive-numbers-sum/0829-consecutive-numbers-sum.java
+++ 0829-consecutive-numbers-sum/0829-consecutive-numbers-sum.java
@@ -8,4 +8,20 @@ class Solution {
         }
         return ans;
     }
+    /*k = no of elemnt 
+    k =1 =15
+    k= 2 = 15 =8+7
+    k =3  = 15 = 4 +5 =6;
+    ....
+
+        x  +x+1+ x+2 .... .x+k-1 = N
+        x +x +x ... k times   + (1 + 2 +3 .... k-1) = N;
+        kx + (k *(k-1)/2) =N;
+        x = N - (k * (k-1) /2 )/k;
+        x > 0
+        N- (k * (k-1)/2) > 0
+        2N  > k(k-1);
+
+        
+    */
 }
