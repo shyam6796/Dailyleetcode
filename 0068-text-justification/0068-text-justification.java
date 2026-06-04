ec795afdba5a9036ba7411683ca324ebacf7f9d1
shyam6796 <44537526+shyam6796@users.noreply.github.com>
2026-06-03 22:43:08 -0500

    Time: 2 ms (20.52%), Space: 43.5 MB (12.04%) - LeetHub

--- 0068-text-justification/0068-text-justification.java
+++ 0068-text-justification/0068-text-justification.java
@@ -1,41 +1,47 @@
 class Solution {
     public List<String> fullJustify(String[] words, int maxWidth) {
         List<String> list = new ArrayList<>();
-       
-        int index =0;
+        int index = 0;
 
-        while(index < words.length){
-            int i=index+1;
-            int totalchars = words[index].length();
-            while(i < words.length  && totalchars + words[i].length() +1 <=maxWidth){
-                totalchars +=words[i].length()+1;
+        while (index < words.length) {
+            int totalChars = words[index].length();
+            int i = index + 1;
+
+            while (i < words.length && totalChars + 1 + words[i].length() <= maxWidth) {
+                totalChars += words[i].length() + 1;
                 i++;
             }
-            int count =i-index-1;
-             StringBuilder sb =new StringBuilder();
-            if(count==0 || i ==words.length){
-                
-                for(int j =index; j<i; j++){
-                    sb.append(words[j] +" ");
+            //totalChars =10 , index =0, i =3
+            int gaps = i - index - 1;//2
+
+            StringBuilder sb = new StringBuilder();
+            if (gaps == 0 || i == words.length) {
+                for (int j = index; j < i; j++) {
+                    sb.append(words[j] + " ");
                 }
-                sb.deleteCharAt(sb.length()-1);
-                while(sb.length() <maxWidth)sb.append(" ");
-            }else{
-                //int count =i-index-1;
-                int evenSpaces =(maxWidth-totalchars)/count;
-                int oddSpaces = (maxWidth-totalchars)%count;
-                 for(int j =index; j<i-1; j++){
-                    sb.append(words[j] +" ");
-                    sb.append(" ".repeat(evenSpaces));
-                    if(oddSpaces-- >0)sb.append(' ');
+                sb.deleteCharAt(sb.length() - 1);
+                while (sb.length() < maxWidth)
+                    sb.append(" ");
+            } else {
+                int minExtraSpace = (maxWidth - totalChars) / gaps;
+                int oddExtraSpace = (maxWidth - totalChars) % gaps;
+
+                for (int j = index; j < i - 1; j++) {
+                    sb.append(words[j] + " ");
+                    sb.append(" ".repeat(minExtraSpace));
+                    if (oddExtraSpace-- > 0)
+                        sb.append(" ");
                 }
-                sb.append(words[i-1]);
+                sb.append(words[i - 1]);
+
             }
+
+           // System.out.println(sb.toString());
+
             list.add(sb.toString());
+            //
             index =i;
         }
-
         return list;
-    
     }
 }
